43a7dfbe923a04ca36547439672df91c9edff412
yogjin <youngjin988@gmail.com>
2023-04-11 17:18:58 +0900

    style: Header 컴포넌트 스타일 추가
    공통 스타일은 global-style.ts에 정의하고 App.tsx에서 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Component } from 'react';
 import { Header, Modal, RestaurantItems, Tab } from './components';
+import { GlobalStyle } from './global-style';
+// import './index.css';
 
-import './index.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header></Header>
-        <Tab></Tab>
-        <RestaurantItems></RestaurantItems>
-        <Modal></Modal>
-      </div>
+      <>
+        <GlobalStyle />
+        <div className="App">
+          <Header></Header>
+          <Tab></Tab>
+          <RestaurantItems></RestaurantItems>
+          <Modal></Modal>
+        </div>
+      </>
     );
   }
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,52 @@
 import { Component } from 'react';
+import styled from 'styled-components';
+import AddButtonImage from '../../assets/add-button.png';
 
 export class Header extends Component {
   render() {
     return (
-      <div className="header">
-        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-        <button type="button" className="gnb__button" aria-label="음식점 추가">
-          <img src="/assets/add-button.png" alt="음식점 추가" />
-        </button>
-      </div>
+      <HeaderWrapper>
+        <Title>점심 뭐 먹지</Title>
+        <AddRestaurantButton type="button" aria-label="음식점 추가">
+          <img src={AddButtonImage} alt="음식점 추가" />
+        </AddRestaurantButton>
+      </HeaderWrapper>
     );
   }
 }
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+
+  padding: 0 16px;
+
+  background-color: var(--primary-color);
+  position: sticky;
+  top: 0;
+`;
+
+const Title = styled.h1`
+  color: #fcfcfd;
+  font: var(--text-title);
+`;
+
+const AddRestaurantButton = styled.button`
+  height: 40px;
+
+  border: none;
+  border-radius: 8px;
+  background: transparent;
+
+  font-size: 24px;
+  cursor: pointer;
+
+  img {
+    display: block;
+    width: 40px;
+    height: 40px;
+    object-fit: contain;
+  }
+`;
