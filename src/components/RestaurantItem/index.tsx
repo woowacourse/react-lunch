079c638fbc7114692c8363fe6a343d6069480e72
woo-jk <201702042@o.cnu.ac.kr>
2023-04-11 17:54:08 +0900

    feat: RestaurantItem 컴포넌트 퍼블리싱 구현

---
+++ src/components/RestaurantItem/index.tsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import { Component } from "react";
+import { CategoryImagePath } from "../../data/CategoryImagePath";
+import { RestaurantProps } from "../../types/restaurant";
+
+export default class RestaurantItem extends Component<RestaurantProps> {
+  render() {
+    return (
+      <li className="restaurant">
+        <div className="restaurant__category">
+          <img src={CategoryImagePath[this.props.category]} alt={this.props.category} className="category-icon" />
+        </div>
+        <div className="restaurant__info">
+          <h3 className="restaurant__name text-subtitle">{this.props.name}</h3>
+          <span className="restaurant__distance text-body">캠퍼스부터 {this.props.distance}분 내</span>
+          <p className="restaurant__description text-body">{this.props.description}</p>
+        </div>
+      </li>
+    );
+  }
+}
