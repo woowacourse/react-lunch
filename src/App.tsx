69ebbc67c639390e90c1efd383cde99b3f28adac
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-11 15:28:19 +0900

    feat: 초기 cra 설정

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import GlobalStyle from './style/global';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
