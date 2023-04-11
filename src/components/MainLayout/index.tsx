4db2e543a38c9f6d7c70e59a2d187ad890bc2aa7
Suyoung <jsy8492@gmail.com>
2023-04-11 17:33:55 +0900

    feat: 음식점 목록 컴포넌트 작성

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -3,13 +3,14 @@ import { Component } from 'react';
 import './style.css';
 
 import FilterSection from './FilterSection';
+import RestaurantListSection from './RestaurantListSection';
 
 class MainLayout extends Component {
 	render() {
 		return (
 			<main>
 				<FilterSection />
-				{/* <RestaurantListSection /> */}
+				<RestaurantListSection />
 			</main>
 		);
 	}
