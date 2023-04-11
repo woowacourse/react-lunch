43349c45cd90d5d3a9d3b913baad53220c10d707
woo-jk <201702042@o.cnu.ac.kr>
2023-04-11 18:36:01 +0900

    feat: mockData 생성 후 적용

--- src/components/RestaurantList/index.tsx
+++ src/components/RestaurantList/index.tsx
@@ -2,30 +2,12 @@ import "./index.css";
 import { Component } from "react";
 import RestaurantItem from "../RestaurantItem";
 import { RestaurantProps } from "../../types/restaurant";
-
-const dummyData: RestaurantProps[] = [
-  {
-    id: 1,
-    name: "test1",
-    category: "한식",
-    distance: 5,
-  },
-  {
-    id: 2,
-    name: "test2",
-    category: "중식",
-    distance: 10,
-  },
-  {
-    id: 3,
-    name: "test3",
-    category: "한식",
-    distance: 5,
-  },
-];
+import mockData from "../../data/mockData.json";
 
 export default class RestaurantList extends Component {
   render() {
+    const dummyData: RestaurantProps[] = JSON.parse(JSON.stringify(mockData.restaurants));
+
     return (
       <section className="restaurant-list-container">
         <ul className="restaurant-list">
