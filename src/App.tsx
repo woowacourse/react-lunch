902649412dab9b8bfcf77a850ddb3269b72b9e98
Suyoung <jsy8492@gmail.com>
2023-04-11 15:44:06 +0900

    feat: Header 컴포넌트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Component } from 'react';
+
 import './css/App.css';
 
-function App() {
-	return <div className="App">점심 뭐 먹지</div>;
+import Header from './components/Header';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<main />
+			</div>
+		);
+	}
 }
 
 export default App;
