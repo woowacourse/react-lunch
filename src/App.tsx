622243d67e4edf92e8b8a71235c44b9bf435d811
hozzijeong <ghwls0905@naver.com>
2023-04-11 21:04:40 +0900

    feat: 컴포넌트 선언 props 할당

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,52 @@ import Selector from './components/Selector';
 import mockData from './mockData/restaurantList.json';
 import { appState, restaurant } from './utils/interfaces';
 import { parseJson } from './utils/json';
+import { FILTERS, RESTAURANT_CATEGORY, WHOLE_CATEGORY, selectorCategory, selectorFilter } from './utils/types';
 
 class App extends React.Component {
   state: appState = {
     category: '전체',
     filter: '이름순',
     wholeList: parseJson<Array<restaurant>>(JSON.stringify(mockData.restaurantList)),
+    currentList: parseJson<Array<restaurant>>(JSON.stringify(mockData.restaurantList)),
   };
 
+  sortingByCategory(category: selectorCategory) {
+    const { wholeList } = this.state;
+    if (category === '전체') {
+      this.setState({ ...this.state, currentList: wholeList });
+      return;
+    }
+
+    this.setState({ ...this.state, currentList: wholeList.filter(item => item.category === category) });
+  }
+
   render(): React.ReactNode {
+    const categoryOptions = [WHOLE_CATEGORY, ...Object.values(RESTAURANT_CATEGORY)];
+    const filterOptions = [...Object.values(FILTERS)];
+
     return (
       <>
         <Header />
         <div>
-          <Selector />
-          <Selector />
+          <Selector<selectorCategory>
+            selectedValue={this.state.category}
+            optionList={categoryOptions}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+              const { value } = e.target;
+
+              this.sortingByCategory(value as selectorCategory);
+            }}
+          />
+          <Selector<selectorFilter>
+            selectedValue={this.state.filter}
+            optionList={filterOptions}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+              const { value } = e.target;
+            }}
+          />
         </div>
-        <ItemList />
+        <ItemList itemList={this.state.currentList} />
       </>
     );
   }
