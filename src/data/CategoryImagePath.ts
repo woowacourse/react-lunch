39346d6747884c62cf5f87fb2fa3e478a458ce89
woo-jk <201702042@o.cnu.ac.kr>
2023-04-11 17:52:42 +0900

    feat: 카테고리 이미지 파일 저장 및 경로 객체 생성

---
+++ src/data/CategoryImagePath.ts
@@ -0,0 +1,8 @@
+export const CategoryImagePath = {
+  한식: `${process.env.PUBLIC_URL}/images/category-korean.png`,
+  중식: `${process.env.PUBLIC_URL}/images/category-chinese.png`,
+  일식: `${process.env.PUBLIC_URL}/images/category-japanese.png`,
+  아시안: `${process.env.PUBLIC_URL}/images/category-asain.png`,
+  양식: `${process.env.PUBLIC_URL}/images/category-western.png`,
+  기타: `${process.env.PUBLIC_URL}/images/category-etc.png`,
+};
