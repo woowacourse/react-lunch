dc458c485b9c081ce0715db07bbc63094176d823
minjaeKim <smallkdb@gmail.com>
2023-04-11 16:31:10 +0900

    feat: Header 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
 	render() {
-		return <div className='App'>초기 설정</div>;
+		return (
+			<main className='App'>
+				<Header />
+			</main>
+		);
 	}
 }
 
