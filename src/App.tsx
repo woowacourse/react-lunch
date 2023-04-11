0518f62274b4f761cad31d0a2d0530b35a0dd2e3
Suyoung <jsy8492@gmail.com>
2023-04-11 15:09:15 +0900

    feat: 공통 스타일시트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import './App.css';
-import './reset.css';
+import './css/App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return <div className="App">점심 뭐 먹지</div>;
 }
 
 export default App;
