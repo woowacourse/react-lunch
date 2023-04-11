b5e6e5d1162e4283ec48162263e60b1fd21a4c70
woo-jk <201702042@o.cnu.ac.kr>
2023-04-11 16:54:08 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  constructor() {
+    super({});
+  }
+
+  render() {
+    return (
+      <header className="gnb">
+        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+      </header>
+    );
+  }
+}
