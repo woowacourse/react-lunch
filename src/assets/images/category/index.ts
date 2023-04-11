57030ef40a622e02271b0494802a7ef1ce8459e2
Leejin-Yang <ejin1996@gmail.com>
2023-04-11 17:23:38 +0900

    style: 기본 스타일 및 이미지 설정
    Co-authored-by: tkdrb12 <tkdrb12@users.noreply.github.com>

---
+++ src/assets/images/category/index.ts
@@ -0,0 +1,19 @@
+import categoryKoreanImage from "./category-korean.png";
+import categoryAsianImage from "./category-asian.png";
+import categoryChineseImage from "./category-chinese.png";
+import categoryJapaneseImage from "./category-japanese.png";
+import categoryWesternImage from "./category-western.png";
+import categoryEtcImage from "./category-etc.png";
+
+import type { Category } from '../../../types/restaurant';
+
+const categoryImages: Readonly<Record<Category, string>> = {
+  한식: categoryKoreanImage,
+  중식: categoryChineseImage,
+  일식: categoryJapaneseImage,
+  아시안: categoryAsianImage,
+  양식: categoryWesternImage,
+  기타: categoryEtcImage,
+};
+
+export default categoryImages;
