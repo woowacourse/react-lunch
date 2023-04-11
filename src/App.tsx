ff7930c2f5194538a8194b54b681840d6b4029bb
NaveOWO <pianowell96@gmail.com>
2023-04-11 14:35:37 +0900

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { GlobalStyle } from "./Globalstyle";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <h1>dlakjfalksd</h1>
+      </>
+    );
+  }
+}
+
+export default App;
