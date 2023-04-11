26a97d5dc67b3a39b00aeb2e6b2774020bf6216e
ashleysyheo <ashleysyheo@gmail.com>
2023-04-11 17:44:31 +0900

    feat: Restaurant 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,21 @@
+export type Category = '중식' | '한식' | '일식' | '아시안식' | '양식' | '기타';
+
+export interface Restaurant {
+  category: Category;
+  name: string;
+  distance: number;
+  description?: string;
+  link?: string;
+  id: number;
+}
+
 interface CurrentDisplayStatus {
   category: string;
   sortBy: string;
 }
 
 export interface State {
-  restaurantList: [];
+  restaurantList: Restaurant[];
   selectedRestaurant: number;
   currentDisplayStatus: CurrentDisplayStatus;
 }
