a0701d2e31137962f9ffa673b8d5be71ad0995dc
Suyoung <jsy8492@gmail.com>
2023-04-11 17:53:46 +0900

    feat: 음식점 카테고리별 svg 아이콘 컴포넌트 작성

---
+++ src/components/common/CategoryIcon/index.tsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+import ChineseIcon from './ChineseIcon';
+import KoreanIcon from './KoreanIcon';
+import EtcIcon from './EtcIcon';
+import AsianIcon from './AsianIcon';
+import WesternIcon from './WesternIcon';
+import JapaneseIcon from './JapaneseIcon';
+
+interface Props {
+	category: string;
+}
+
+class CategoryIcon extends Component<Props> {
+	render() {
+		switch (this.props.category) {
+			case '한식':
+				return <KoreanIcon />;
+			case '중식':
+				return <ChineseIcon />;
+			case '일식':
+				return <JapaneseIcon />;
+			case '양식':
+				return <WesternIcon />;
+			case '아시안':
+				return <AsianIcon />;
+			case '기타':
+				return <EtcIcon />;
+		}
+	}
+}
+
+export default CategoryIcon;
