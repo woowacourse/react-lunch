4b5e9660bc302c513993ab6ad6d360ef71ba719d
ashleysyheo <ashleysyheo@gmail.com>
2023-04-11 17:45:49 +0900

    feat: props를 통해 음식점 목록 데이터 전달하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Component, ReactNode } from 'react';
 import './App.css';
 import RestaurantList from './components/RestaurantList';
-import { State } from './types';
+import { Restaurant, State } from './types';
+import mockData from './data/mockRestaurantData.json';
 
 class App extends Component {
   state: State = {
-    restaurantList: [],
+    restaurantList: mockData.restaurants as Restaurant[], // mock data get data ?? mock data
     selectedRestaurant: 0,
     currentDisplayStatus: {
       category: '',
@@ -14,11 +15,13 @@ class App extends Component {
   };
 
   render(): ReactNode {
+    const { restaurantList } = this.state;
+
     return (
       <div className="App">
         {/* <Header></Header>
         <FilterSection></FilterSection> */}
-        <RestaurantList></RestaurantList>
+        <RestaurantList restaurantList={restaurantList} />
       </div>
     );
   }
