b8f5a6df4c65c8686930a006f808e6a2b401d683
NaveOWO <pianowell96@gmail.com>
2023-04-11 17:41:11 +0900

    feat : category 타입 export

--- src/types/restaurant.ts
+++ src/types/restaurant.ts
@@ -7,5 +7,12 @@ export interface RestaurantItemPropsType {
   link: string;
 }
 
-type Category = "전체" | "일식" | "양식" | "한식" | "아시안" | "중식" | "기타";
+export type Category =
+  | "전체"
+  | "일식"
+  | "양식"
+  | "한식"
+  | "아시안"
+  | "중식"
+  | "기타";
 type TakingTime = 5 | 10 | 15 | 20 | 25 | 30;
