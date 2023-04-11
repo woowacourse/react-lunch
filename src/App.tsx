46f1b53105464b3d024e9d3ebf4b4ded38ea8ba4
Gilpop8663 <wolfye@naver.com>
2023-04-11 16:38:14 +0900

    chore: 프로젝트, eslint, prettier 초기 설정
    Co-authored-by: WaiNaat <qoli0703@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
