1b15dd737548a4dc9dfcadf48877b6255701c457
ashleysyheo <ashleysyheo@gmail.com>
2023-04-11 17:44:08 +0900

    test: 음식점 목록 테스트 추가

--- src/__tests__/render.test.jsx
+++ src/__tests__/render.test.jsx
@@ -1,17 +1,47 @@
 import { render, screen } from '@testing-library/react';
 import RestaurantItem from '../components/RestaurantItem';
+import RestaurantList from '../components/RestaurantList';
+import mockData from '../data/mockRestaurantData';
 
 describe('음식점 리스트 렌더링 테스트', () => {
-  test('음식점 아이템이 렌더링된다', () => {
-    render(<RestaurantItem />);
+  const restaurant = {
+    category: '중식',
+    name: '찐친',
+    distance: 5,
+    description: '찐친들이랑 가는 중국집',
+    link: '',
+    id: 1,
+  };
 
-    expect(screen.getByText('캠퍼스부터')).toBeInTheDocument();
+  test('음식점 아이템이 렌더링된다', () => {
+    render(<RestaurantItem restaurant={restaurant} />);
+
+    expect(screen.getByText('캠퍼스부터 5분 내')).toBeInTheDocument();
   });
 
-  // 카테고리 이미지, 음식점 이름, 거리, 정보 내용을 담고 있다
-  // test('음식점 목록이 렌더링 된다.', () => {
-  //   render(<App />);
+  test('음식점 아이템은 카테고리 이미지, 음식점 이름, 거리, 정보 내용을 담고 있다', () => {
+    render(<RestaurantItem restaurant={restaurant} />);
+
+    expect(screen.getByAltText('중식')).toBeInTheDocument();
+    expect(screen.getByText('찐친')).toBeInTheDocument();
+    expect(screen.getByText('캠퍼스부터 5분 내')).toBeInTheDocument();
+    expect(screen.getByText('찐친들이랑 가는 중국집')).toBeInTheDocument();
+  });
+
+  test('초기 데이터 10개의 음식이 목록에 있다', () => {
+    const restaurantList = mockData.restaurants;
+
+    render(<RestaurantList restaurantList={restaurantList} />);
+
+    restaurantList.forEach((restaurant) => {
+      expect(screen.getAllByAltText(restaurant.category)).toBeTruthy();
+      expect(screen.getAllByText(`캠퍼스부터 ${restaurant.distance}분 내`)).toBeTruthy();
+      expect(screen.getByText(restaurant.name)).toBeInTheDocument();
+      expect(screen.getByText(restaurant.description)).toBeInTheDocument();
+    });
+  });
+
+  // test('처음 방문시 전체 카테고리, 이름순으로 정렬되어 있다', () => {
+
   // });
-  // 초기 데이터 10개의 음식이 목록에 있다
-  // 처음 방문시 전체 카테고리, 이름순으로 정렬되어 있다
 });
