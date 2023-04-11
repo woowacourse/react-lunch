30c8880927f0e41e0bac41c7e9d2b3a98dd42f53
woo-jk <201702042@o.cnu.ac.kr>
2023-04-11 17:06:28 +0900

    feat: Filter 컴포넌트 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Filter from "./components/Filter";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Filter />
     </div>
   );
 }
