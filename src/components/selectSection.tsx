649d5e890131ed439a001873c9d986fb0e59237c
NaveOWO <pianowell96@gmail.com>
2023-04-11 16:38:31 +0900

    feat : select컴포넌트에 props전달

--- src/components/selectSection.tsx
+++ src/components/selectSection.tsx
@@ -5,12 +5,24 @@ import { Select } from "./select";
 const CATEGORIES = ["전체", "일식", "양식", "한식", "아시안", "중식", "기타"];
 const SORTS = ["이름순", "거리순"];
 
-export class SelectSection extends React.Component {
+export class SelectSection extends React.Component<any> {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+
   render() {
     return (
       <Container>
-        <Select options={CATEGORIES} />
-        <Select options={SORTS} />
+        <Select
+          type="category"
+          options={CATEGORIES}
+          handleSelect={this.props.handleSelect}
+        />
+        <Select
+          type="sorting"
+          options={SORTS}
+          handleSelect={this.props.handleSelect}
+        />
       </Container>
     );
   }
