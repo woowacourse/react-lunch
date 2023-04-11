a55c6b8b939025e7d11e41026ff25c6d0e8bb856
hozzijeong <ghwls0905@naver.com>
2023-04-11 17:55:28 +0900

    feat: AppState의 초기 데이터 값 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import ItemList from './components/ItemList';
+import Selector from './components/Selector';
+import mockData from './mockData/restaurantList.json';
+import { appState, restaurant } from './utils/interfaces';
+import { parseJson } from './utils/json';
 
 class App extends React.Component {
+  state: appState = {
+    category: '전체',
+    filter: '이름순',
+    wholeList: parseJson<Array<restaurant>>(JSON.stringify(mockData.restaurantList)),
+  };
+
   render(): React.ReactNode {
     return (
       <>
         <Header />
+        <div>
+          <Selector />
+          <Selector />
+        </div>
+        <ItemList />
       </>
     );
   }
