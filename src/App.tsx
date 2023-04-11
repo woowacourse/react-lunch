5a04d72c2922c07ffa27cfad60843d09505b8fec
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-11 16:04:03 +0900

    style: prettier 설정 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return <div className='App'>안녕하세요</div>
+  return <div className="App">안녕하세요</div>;
 }
 
-export default App
+export default App;
