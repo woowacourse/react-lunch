6ce62306b3f45778e1bf0f7eaf47266ad8812fad
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-11 16:00:35 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <>
+        <div>점심 뭐먹지</div>
+
+        <div>정렬, 필터</div>
+
+        <div>음식점 목록</div>
+      </>
+    );
+  }
+}
+
+export default App;
