f916f9e28d66b37cfd239211b0d3a4ae7bff17e4
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-11 21:47:47 +0900

    refactor: mockData.json파일 생성 및 적용
    기존 app내에 있던 mockData배열 json파일로 개선

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,21 @@ import Header from './components/Header';
 import FilterContainer from './components/FilterContainter';
 import RestaurantList from './components/RestaurantList';
 import Modal from './components/Modal';
-import './App.css';
 import { Restaurant } from './types';
-
-const mockData = [
-  {
-    category: '중식',
-    name: '차이나타운',
-    id: 123,
-    description: '토끼의 중식 맛집',
-    distance: '10',
-    link: '',
-  },
-] as Restaurant[];
+import mockData from './mockData.json';
+import './App.css';
 
 export default class App extends Component {
+  state = {
+    restaurants: mockData as Restaurant[],
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
         <FilterContainer />
-        <RestaurantList restaurants={mockData} />
+        <RestaurantList restaurants={this.state.restaurants} />
         <Modal />
       </div>
     );
