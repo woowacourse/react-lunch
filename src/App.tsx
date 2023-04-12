1aa6039933a5eee22a7f7393fc733e1e82a279d1
Gilpop8663 <wolfye@naver.com>
2023-04-12 18:13:10 +0900

    feat: 앱 초기화면 UI 구현
    Co-authored-by: WaiNaat <qoli0703@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles/App.css';
+import { RestaurantInfo } from './types/restaurantInfo';
+import { getSavedRestaurantList, hasSavedRestaurantList } from './domain/initializeRestaurantList';
+import { saveNewRestaurantList } from './domain/initializeRestaurantList';
+import RestaurantList from './components/RestaurantList';
 
-class App extends Component {
-  constructor(props: any) {
+class App extends Component<object, { restaurantList: RestaurantInfo[] }> {
+  constructor(props: object) {
     super(props);
+
+    this.state = {
+      restaurantList: [] as RestaurantInfo[],
+    };
+  }
+
+  async componentDidMount() {
+    if (!hasSavedRestaurantList()) await saveNewRestaurantList();
+
+    const list = getSavedRestaurantList();
+
+    this.setState({
+      restaurantList: list,
+    });
   }
 
   render() {
-    return <div></div>;
+    const { restaurantList } = this.state;
+    return (
+      <div className="app">
+        <Header />
+        <RestaurantList restaurantList={restaurantList} />
+      </div>
+    );
   }
 }
 
