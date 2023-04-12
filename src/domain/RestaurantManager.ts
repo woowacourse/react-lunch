b24cd869582eead43976ab6771d69cb893cc6e8b
semnil5202 <semnil5202@gmail.com>
2023-04-12 17:32:43 +0900

    feat: 카테고리 및 이름순, 거리순으로 정렬하는 기능 추가
    Co-authored-by: ukkodeveloper <ukkodeveloper@users.noreply.github.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -1,32 +1,52 @@
-export const data = [
-  {
-    category: '한식',
-    name: '피양콩',
-    distance: 10,
-    description: '맛나용',
-  },
-  {
-    category: '한식',
-    name: '피양콩',
-    distance: 10,
-    description: '맛나용',
-  },
-  {
-    category: '한식',
-    name: '피양콩',
-    distance: 10,
-    description: '맛나용',
-  },
-  {
-    category: '한식',
-    name: '피양콩',
-    distance: 10,
-    description: '맛나용',
-  },
-  {
-    category: '한식',
-    name: '피양콩',
-    distance: 10,
-    description: '맛나용',
-  },
-];
+import mockData from './mockData.json';
+import { RestaurantDetail, Category } from '../types/RestaurantDetail';
+
+const RESTAURANT_LOCAL_STORAGE_ID = 'restaurantList';
+
+// local storage 접근
+const initMockData = () => {
+  localStorage.setItem(RESTAURANT_LOCAL_STORAGE_ID, JSON.stringify(mockData));
+};
+
+const getRestaurantList = (): RestaurantDetail[] => {
+  const restaurantList: RestaurantDetail[] = JSON.parse(
+    localStorage.getItem(RESTAURANT_LOCAL_STORAGE_ID) || '{}'
+  );
+
+  return restaurantList;
+};
+
+const sortByName = (restaurantList: RestaurantDetail[]) => {
+  return restaurantList.sort((a, b) => a.name.localeCompare(b.name));
+};
+
+const sortByDistance = (restaurantList: RestaurantDetail[]) => {
+  return restaurantList.sort((a, b) => a.distance - b.distance);
+};
+
+const filterByCategory =
+  (_category: Category) => (restaurantList: RestaurantDetail[]) => {
+    if (_category === '전체') return restaurantList;
+
+    return restaurantList.filter(
+      (restaurant) => restaurant.category === _category
+    );
+  };
+
+const getFiltered = (
+  filterFnList: ((restaurant: RestaurantDetail[]) => RestaurantDetail[])[],
+  restaurantList: RestaurantDetail[]
+) => {
+  return filterFnList.reduce((restaurantList, fn) => {
+    return fn(restaurantList);
+  }, restaurantList);
+};
+
+export default {
+  initMockData,
+  getRestaurantList,
+  sortByName,
+  sortByDistance,
+  filterByCategory,
+  getFiltered,
+};
--- src/types/RestaurantDetail.ts
+++ src/types/RestaurantDetail.ts
@@ -1,6 +1,17 @@
-export default interface RestaurantDetail {
-  category: string;
+export interface RestaurantDetail {
+  id: number;
+  category: Category;
   name: string;
   distance: number;
   description: string;
+  link?: string;
 }
+
+export type Category =
+  | '전체'
+  | '한식'
+  | '중식'
+  | '일식'
+  | '양식'
+  | '아시안'
+  | '기타';
