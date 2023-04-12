c3fa7159f986135a6d3822134e26f92792f44481
Dain Lee <feb.dain@gmail.com>
2023-04-12 14:23:59 +0900

    fix: App.test.js 에러 개선
    `expect(...).toBeInTheDocument is not a function` error fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import "@testing-library/jest-dom/extend-expect";
 
-test("renders learn react link", () => {
+test("renders learn react link", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("닫기");
   expect(linkElement).toBeInTheDocument();
 });
