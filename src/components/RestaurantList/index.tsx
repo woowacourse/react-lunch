0f97f456745ac2775209166bad0933c72d547433
woo-jk <201702042@o.cnu.ac.kr>
2023-04-12 15:56:42 +0900

    feat: 카테고리별 필터링 기능, 정렬 기능 구현

--- src/components/RestaurantList/index.tsx
+++ src/components/RestaurantList/index.tsx
@@ -8,18 +8,61 @@ interface RestaurantListProps {
   selectedCategory: CategoryOption;
   selectedSort: SortOption;
 }
-export default class RestaurantList extends Component<RestaurantListProps> {
-  render() {
-    const dummyData: Restaurant[] = JSON.parse(JSON.stringify(mockData.restaurants));
 
+interface RestaurantListState {
+  filteredRestaurants: Restaurant[];
+}
+export default class RestaurantList extends Component<RestaurantListProps> {
+  allRestaurants: Restaurant[] = JSON.parse(JSON.stringify(mockData.restaurants));
+
+  state: RestaurantListState = {
+    filteredRestaurants: this.sort(this.allRestaurants),
+  };
+
+  componentDidUpdate(prevProps: RestaurantListProps) {
+    if (prevProps.selectedCategory !== this.props.selectedCategory) {
+      const updatedRestaurants = this.filter();
+      this.setState({ filteredRestaurants: this.sort(updatedRestaurants) });
+    }
+    if (prevProps.selectedSort !== this.props.selectedSort) {
+      this.setState({ filteredRestaurants: this.sort(this.state.filteredRestaurants) });
+    }
+  }
+
+  render() {
     return (
       <section className="restaurant-list-container">
         <ul className="restaurant-list">
-          {dummyData.map((restaurantData) => (
+          {this.state.filteredRestaurants.map((restaurantData) => (
             <RestaurantItem key={restaurantData.id} {...restaurantData} />
           ))}
         </ul>
       </section>
     );
+  }
+
+  filter() {
+    const { selectedCategory } = this.props;
+
+    if (selectedCategory === "all") {
+      return this.state.filteredRestaurants;
+    }
+
+    const allRestaurants = this.allRestaurants.filter((restaurant) => restaurant.category === selectedCategory);
+
+    return allRestaurants;
+  }
+
+  sort(allRestaurants: Restaurant[]) {
+    const condition = this.props.selectedSort;
+
+    if (condition === "name") {
+      allRestaurants.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    if (condition === "distance") {
+      allRestaurants.sort((a, b) => a.distance - b.distance);
+    }
+
+    return allRestaurants;
   }
 }
