e3a882369d6e3dae2c170469562c85da54590bda
Suyoung <jsy8492@gmail.com>
2023-04-12 14:26:34 +0900

    refactor: 음식점 목록 데이터를 props로 전달하도록 수정

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -5,12 +5,16 @@ import './style.css';
 import FilterSection from './FilterSection';
 import RestaurantListSection from './RestaurantListSection';
 
-class MainLayout extends Component {
+import { Restaurant } from '../../domain/type';
+interface Props {
+  restaurants: Restaurant[] | null;
+}
+class MainLayout extends Component<Props> {
   render() {
     return (
       <main>
         <FilterSection />
-        <RestaurantListSection />
+        <RestaurantListSection restaurants={this.props.restaurants} />
       </main>
     );
   }
