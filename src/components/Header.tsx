476d7749c0cb626fff92021b876e7d883d5e00ca
ashleysyheo <ashleysyheo@gmail.com>
2023-04-12 16:19:28 +0900

    feat: Header 컴포넌트 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <a href="">
+        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+      </a>
+    );
+  }
+}
+
+export default Header;
