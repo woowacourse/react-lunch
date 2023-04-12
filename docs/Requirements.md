35ef4113c58f5a8d2095c9c2fa0b3032aff6a87d
kyw0716 <kyw0716@naver.com>
2023-04-12 11:44:46 +0900

    feat: App 컴포넌트 localStorage 저장 기능 추가
    Co-authored-by: Taeeun Kim <taeeun.dev@gmail.com>

--- docs/Requirements.md
+++ docs/Requirements.md
@@ -1,12 +1,11 @@
 ## 컴포넌트 목록
 
-- [x] Header 컴포넌트
-- [ ] SelectBox 컴포넌트
+- [ ] App 컴포넌트
+  - [x] localStorage에 레스토랑 정보 저장
   - [ ] 이름순, 거리순 정렬 기능
   - [ ] 카테고리 필터링 기능
+- [ ] SelectBox 컴포넌트
 - [x] RestaurantItem 컴포넌트
-- [ ] RestaurantList 컴포넌트
-  - [ ] localStorage에 레스토랑 정보 저장
 - [x] Modal 공통 컴포넌트
   - [x] 열고 닫는 기능
 - [x] RestaurantDetailModal 컴포넌트
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { Header } from './components/Header';
+import { RestaurantList } from './components/RestaurantList';
+import dummyList from './mockData.json';
+import { Restaurant } from './type';
+import { Modal } from './components/modal/Modal';
+import { RestaurantDetail } from './components/modal/RestaurantDetail';
 
-function App() {
-  return <div className="App"></div>;
+interface AppState {
+  isOpen: boolean;
+  list: Restaurant[];
+  selectedItem: Restaurant | null;
 }
 
-export default App;
+export class App extends Component<any, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    const localStorageData = localStorage.getItem('restaurantList');
+    const restaurantList =
+      localStorageData !== null ? JSON.parse(localStorageData) : dummyList;
+
+    this.state = {
+      isOpen: false,
+      list: restaurantList as Restaurant[],
+      selectedItem: null,
+    };
+  }
+
+  toggleIsOpen() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  clickRestaurantItem(e: any) {
+    const selectedId = e.target.closest('li').id;
+    const selectedRestaurant = this.state.list.find(
+      ({ id }) => id === selectedId
+    );
+
+    if (selectedRestaurant === undefined) return;
+
+    this.setState({ selectedItem: selectedRestaurant });
+    this.toggleIsOpen();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <GlobalStyle />
+        <Header />
+        <RestaurantList
+          list={this.state.list}
+          clickRestaurantItem={this.clickRestaurantItem.bind(this)}
+        />
+        <Modal
+          isOpen={this.state.isOpen}
+          children={
+            this.state.selectedItem && (
+              <RestaurantDetail info={this.state.selectedItem} />
+            )
+          }
+          toggleOpen={this.toggleIsOpen.bind(this)}
+        />
+      </div>
+    );
+  }
+}
