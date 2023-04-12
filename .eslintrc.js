3f50720d2ad4e4eda22382a10990b2c177ffdb22
Gilpop8663 <wolfye@naver.com>
2023-04-12 15:32:35 +0900

    chore: eslint 클래스형 컴포넌트 허용 설정
    Co-authored-by: WaiNaat <qoli0703@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'react/prop-types': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'react/prefer-stateless-function': 0,
   },
 };
