f68b69b7a4b4628038239942c6080fac896cff0e
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-12 16:23:05 +0900

    feat: 초기 렌더링 시 로컬스토리지 확인 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,19 @@ type AppState = { restaurant: Restaurant[]; category: Category; sort: SortBy };
 class App extends React.Component<any, AppState> {
   constructor(props: any) {
     super(props);
-    const { restaurant } = mockData as { restaurant: Restaurant[] };
-    this.state = { restaurant, category: "전체", sort: "이름순" };
+
+    const storageData = localStorage.getItem("restaurant");
+    if (storageData === null) {
+      const { restaurant } = mockData as { restaurant: Restaurant[] };
+      localStorage.setItem("restaurant", JSON.stringify(restaurant));
+      this.state = { restaurant, category: "전체", sort: "이름순" };
+    } else {
+      this.state = {
+        restaurant: JSON.parse(storageData),
+        category: "전체",
+        sort: "이름순",
+      };
+    }
 
     this.setCategory = this.setCategory.bind(this);
     this.setSort = this.setSort.bind(this);
