931c4e0219a47cee1a8eda8b2fb0e5ef35c161f0
NaveOWO <pianowell96@gmail.com>
2023-04-12 11:31:23 +0900

    feat : mockData를 컴포넌트에 state로 생성하는 기능 구현

--- src/components/restaurantSection.tsx
+++ src/components/restaurantSection.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { getRestaurantData } from "../api/getData";
 import { RestaurantItemPropsType } from "../types/restaurant";
 import { RestaurantItem } from "./restaurantItem";
 
-export class RestaurantSection extends React.Component<{
-  restaurants: RestaurantItemPropsType[];
-}> {
+export class RestaurantSection extends React.Component<
+  any,
+  {
+    restaurants: RestaurantItemPropsType[];
+  }
+> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      restaurants: [],
+    };
+  }
+
+  async componentDidMount() {
+    const data = await getRestaurantData();
+
+    this.setState({
+      restaurants: data,
+    });
+  }
+
   render() {
     return (
       <RestaurantContainer>
-        {this.props.restaurants.map((restaurant: RestaurantItemPropsType) => (
+        {this.state.restaurants.map((restaurant: RestaurantItemPropsType) => (
           <RestaurantItem key={restaurant.id} {...restaurant} />
         ))}
       </RestaurantContainer>
