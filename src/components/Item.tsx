a91da240b447e6455d81e13af5e0cd655b2691c3
hozzijeong <ghwls0905@naver.com>
2023-04-12 14:02:11 +0900

    feat: 카테고리 이미지 추가 및 거리와 설명 출력

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { restaurant } from '../utils/interfaces';
 import '../styles/Item.css';
+import { CATEGORY_IMAGE_PATH } from '../utils/constants';
 
 type itemProps = {
   props: restaurant;
@@ -8,16 +9,18 @@ type itemProps = {
 
 class Item extends React.Component<itemProps> {
   render(): React.ReactNode {
+    const { category, name, distance, description } = this.props.props;
+
     return (
       <li className="restaurant" id="restaurant${restaurantInfo.id}">
         <div className="restaurant_info">
           <div className="restaurant__category">
-            {/* <img alt="${restaurantInfo.category}" className="category-icon"> */}
+            <img src={CATEGORY_IMAGE_PATH[category]} alt={category} className="category-icon" />
           </div>
           <div className="restaurant__info">
-            <h3 className="restaurant__name text-subtitle">{this.props.props.name}</h3>
-            <span className="restaurant__distance text-body">캠퍼스로부터 {}분 내</span>
-            <p className="restaurant__description text-body">{}</p>
+            <h3 className="restaurant__name text-subtitle">{name}</h3>
+            <span className="restaurant__distance text-body">캠퍼스로부터 {distance}분 내</span>
+            <p className="restaurant__description text-body">{description}</p>
           </div>
         </div>
       </li>
