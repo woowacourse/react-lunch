91630691ab89e034f55353a66950fd0df719c76a
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-12 16:47:05 +0900

    feat: 음식점 기본 랜더 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import { Component } from 'react';
+import { Component, PropsWithChildren } from 'react';
 import Header from './components/Header';
 import FilterContainer from './components/FilterContainter';
 import RestaurantList from './components/RestaurantList';
 import Modal from './components/Modal';
 import { Restaurant } from './types';
 import mockData from './mockData.json';
+import { getFilteredRestaurantsByCategory, getSortedRestaurants } from './RestaurantUtils';
+import type { RestaurantSortOption, RestaurantCategoryFilterOption } from './RestaurantUtils';
 import './App.css';
 
-export default class App extends Component {
-  state = {
-    restaurants: mockData as Restaurant[],
-  };
+type AppState = {
+  restaurants: Restaurant[];
+  categoryFilterOption: RestaurantCategoryFilterOption;
+  sortOption: RestaurantSortOption;
+};
+
+export default class App extends Component<PropsWithChildren, AppState> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+
+    this.state = {
+      restaurants: mockData as Restaurant[],
+      categoryFilterOption: '전체',
+      sortOption: 'distance',
+    };
+  }
 
   render() {
+    const { restaurants, categoryFilterOption, sortOption } = this.state;
+    const filteredRestaurants = getFilteredRestaurantsByCategory(restaurants, categoryFilterOption);
+    const sortedRestaurants = getSortedRestaurants(filteredRestaurants, sortOption);
+
     return (
       <div className="App">
         <Header />
         <FilterContainer />
-        <RestaurantList restaurants={this.state.restaurants} />
+        <RestaurantList restaurants={sortedRestaurants} />
         <Modal />
       </div>
     );
