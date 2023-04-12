db1de402d8f81466e5ae9354789b7b6f77c5c325
n0eyes <doll4586633@naver.com>
2023-04-12 19:37:19 +0900

    feat: 음식점 필터링 기능 추가

--- react-lunch/src/components/RestaurantList/index.tsx
+++ react-lunch/src/components/RestaurantList/index.tsx
@@ -12,15 +12,48 @@ interface RestaurantListProps {
 }
 
 interface State {
+  restaurantListOrigin: Restaurant[];
   restaurantList: Restaurant[];
 }
 
 class RestaurantList extends Component<RestaurantListProps, State> {
-  state = { restaurantList: [] };
-  componentDidMount() {
-    fetch('./mocks/mockData.json')
+  state = { restaurantListOrigin: [], restaurantList: [] };
+
+  async componentDidMount() {
+    const restaurantList = await fetch('./mocks/mockData.json')
       .then((res) => res.json())
-      .then((res: Restaurant[]) => this.setState({ restaurantList: res }));
+      .then((res: Restaurant[]) => alignBy('이름순', res));
+
+    this.setState({ restaurantListOrigin: restaurantList, restaurantList });
+  }
+
+  componentDidUpdate(prevProps: Readonly<RestaurantListProps>): void {
+    const [prevCategory, prevAlign] = prevProps.filterOptions;
+    const [nextCategory, nextAlign] = this.props.filterOptions;
+
+    if (prevCategory !== nextCategory) {
+      this.filter();
+    }
+
+    if (prevCategory !== nextCategory || prevAlign !== nextAlign) {
+      this.align();
+    }
+  }
+
+  filter() {
+    const [categoryFilter] = this.props.filterOptions;
+
+    this.setState(({ restaurantListOrigin }) => ({
+      restaurantList: filterBy(categoryFilter, restaurantListOrigin),
+    }));
+  }
+
+  align() {
+    const [_, alignFilter] = this.props.filterOptions;
+
+    this.setState(({ restaurantList }) => ({
+      restaurantList: alignBy(alignFilter, restaurantList),
+    }));
   }
 
   render(): ReactNode {
@@ -37,3 +70,25 @@ class RestaurantList extends Component<RestaurantListProps, State> {
 }
 
 export default RestaurantList;
+
+const filterBy = (
+  categoryFilter: CategoryFilter,
+  restaurantList: Restaurant[]
+) => {
+  if (categoryFilter === '전체') return restaurantList;
+
+  return restaurantList.filter(({ category }) => category === categoryFilter);
+};
+
+const alignBy = (alignFilter: AlignFilter, restaurantList: Restaurant[]) => {
+  switch (alignFilter) {
+    case '거리순':
+      return restaurantList.sort((prev, next) => prev.distance - next.distance);
+
+    case '이름순':
+    default:
+      return restaurantList.sort((prev, next) =>
+        prev.title > next.title ? 1 : -1
+      );
+  }
+};
