22bbca779b46cd1fb78d74b34d46156a936bd48b
semnil5202 <semnil5202@gmail.com>
2023-04-12 22:48:22 +0900

    refactor: RestaurantList 렌더링 효율성을 위해 PureComponent 설정
    Co-authored-by: ukkodeveloper <ukkodeveloper@users.noreply.github.com>

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { PureComponent } from 'react';
 import RestaurantItem from './RestaurantItem';
 import RestaurantManager from '../domain/RestaurantManager';
 import { Category } from '../types/RestaurantDetail';
@@ -6,25 +6,29 @@ import { Category } from '../types/RestaurantDetail';
 interface RestuarantListProps {
   category: Category;
   sort: string;
+  onOpenModal: (event: React.MouseEvent<HTMLUListElement>) => void;
 }
 
-export default class RestaurantList extends Component<RestuarantListProps> {
+export default class RestaurantList extends PureComponent<RestuarantListProps> {
   constructor(props: RestuarantListProps) {
     super(props);
   }
 
   render() {
-    const { category, sort } = this.props;
+    const { onOpenModal, category, sort } = this.props;
 
     return (
-      <>
+      <ul onClick={onOpenModal}>
         {RestaurantManager.getRestaurantListFilteredByOptions(
           category,
           sort
         ).map((itemDetail) => (
-          <RestaurantItem detail={itemDetail}></RestaurantItem>
+          <RestaurantItem
+            key={itemDetail.id}
+            detail={itemDetail}
+          ></RestaurantItem>
         ))}
-      </>
+      </ul>
     );
   }
 }
