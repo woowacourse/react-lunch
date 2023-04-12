aa331dbf76be3df9cc0718618bab9f69d7e3d920
ashleysyheo <ashleysyheo@gmail.com>
2023-04-12 16:16:40 +0900

    feat: 필터/정렬 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,8 @@
 const RESTAURANT_CATEGORY = ['중식', '한식', '일식', '아시안식', '양식', '기타'] as const;
+const SORT_BY = ['이름순', '거리순'] as const;
+
+const RESTAURANT_CATEGORY_OPTION = ['전체', ...RESTAURANT_CATEGORY];
 
 const LOCAL_STORAGE_KEY = 'restaurantList';
 
-export { RESTAURANT_CATEGORY, LOCAL_STORAGE_KEY };
+export { RESTAURANT_CATEGORY, SORT_BY, RESTAURANT_CATEGORY_OPTION, LOCAL_STORAGE_KEY };
