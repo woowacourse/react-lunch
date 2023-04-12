5b46d00a63f5273d4a94b7544fcee366060af72a
Suyoung <jsy8492@gmail.com>
2023-04-12 14:25:18 +0900

    feat: 초기 데이터 세팅
    - localStorage에 데이터가 존재하면 초기 값으로 설정한다.
    - localStorage에 데이터가 없으면 mockData.json을 초기 값으로 설정한다.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,34 @@ import './css/App.css';
 
 import Header from './components/Header';
 import MainLayout from './components/MainLayout';
+import { getLocalStorage, setLocalStorage } from './utils/localStorage';
 
 class App extends Component {
+  state = {
+    restaurants: null,
+  };
+
+  async componentDidMount() {
+    const localStorageData = getLocalStorage('restaurants');
+
+    if (localStorageData) {
+      this.setState({ restaurants: localStorageData });
+      return;
+    }
+
+    const response = await fetch('./mockData.json');
+
+    const data = await response.json();
+
+    setLocalStorage('restaurants', data);
+    this.setState({ restaurants: data });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <MainLayout />
+        <MainLayout restaurants={this.state.restaurants} />
       </div>
     );
   }
