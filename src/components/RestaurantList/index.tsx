2744912d26ed9db52f5bb3a4c9bae5cf1abfa60d
woo-jk <201702042@o.cnu.ac.kr>
2023-04-12 16:29:11 +0900

    refactor: RestaurantList 리팩토링(filter, sort 함수 네이밍 변경, sort 함수 분리, 전체적인 변수 네이밍 통일)

--- src/components/RestaurantList/index.tsx
+++ src/components/RestaurantList/index.tsx
@@ -12,57 +12,71 @@ interface RestaurantListProps {
 interface RestaurantListState {
   filteredRestaurants: Restaurant[];
 }
+
 export default class RestaurantList extends Component<RestaurantListProps> {
   allRestaurants: Restaurant[] = JSON.parse(JSON.stringify(mockData.restaurants));
 
   state: RestaurantListState = {
-    filteredRestaurants: this.sort(this.allRestaurants),
+    filteredRestaurants: this.getSortedListByName(this.allRestaurants),
   };
 
   componentDidUpdate(prevProps: RestaurantListProps) {
     if (prevProps.selectedCategory !== this.props.selectedCategory) {
-      const updatedRestaurants = this.filter();
-      this.setState({ filteredRestaurants: this.sort(updatedRestaurants) });
+      const restaurantListByCategory = this.getListByCategory(this.allRestaurants);
+      const sortedRestaurantList = this.getSortedList(restaurantListByCategory);
+
+      this.setState({ filteredRestaurants: sortedRestaurantList });
     }
+
     if (prevProps.selectedSort !== this.props.selectedSort) {
-      this.setState({ filteredRestaurants: this.sort(this.state.filteredRestaurants) });
+      const sortedRestaurantList = this.getSortedList(this.state.filteredRestaurants);
+
+      this.setState({ filteredRestaurants: sortedRestaurantList });
     }
   }
 
+  getListByCategory(restaurants: Restaurant[]) {
+    const { selectedCategory } = this.props;
+
+    if (selectedCategory === "all") {
+      return restaurants;
+    }
+
+    return restaurants.filter((restaurant) => restaurant.category === selectedCategory);
+  }
+
+  getSortedList(restaurants: Restaurant[]) {
+    const { selectedSort } = this.props;
+
+    if (selectedSort === "name") {
+      return this.getSortedListByName(restaurants);
+    }
+    if (selectedSort === "distance") {
+      return this.getSortedListByDistance(restaurants);
+    }
+
+    return restaurants;
+  }
+
+  getSortedListByName(restaurants: Restaurant[]) {
+    return [...restaurants].sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  getSortedListByDistance(restaurants: Restaurant[]) {
+    return [...restaurants].sort((a, b) => a.distance - b.distance);
+  }
+
   render() {
+    const { filteredRestaurants } = this.state;
+
     return (
       <section className="restaurant-list-container">
         <ul className="restaurant-list">
-          {this.state.filteredRestaurants.map((restaurantData) => (
+          {filteredRestaurants.map((restaurantData) => (
             <RestaurantItem key={restaurantData.id} {...restaurantData} />
           ))}
         </ul>
       </section>
     );
-  }
-
-  filter() {
-    const { selectedCategory } = this.props;
-
-    if (selectedCategory === "all") {
-      return this.state.filteredRestaurants;
-    }
-
-    const allRestaurants = this.allRestaurants.filter((restaurant) => restaurant.category === selectedCategory);
-
-    return allRestaurants;
-  }
-
-  sort(allRestaurants: Restaurant[]) {
-    const condition = this.props.selectedSort;
-
-    if (condition === "name") {
-      allRestaurants.sort((a, b) => a.name.localeCompare(b.name));
-    }
-    if (condition === "distance") {
-      allRestaurants.sort((a, b) => a.distance - b.distance);
-    }
-
-    return allRestaurants;
   }
 }
