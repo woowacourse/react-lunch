4b75072ddec2e755ec164e6905caece18d919f21
hozzijeong <ghwls0905@naver.com>
2023-04-12 20:41:07 +0900

    refactor: Item 이벤트 리스너 위임

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { restaurant } from '../utils/interfaces';
 import '../styles/Item.css';
 import { CATEGORY_IMAGE_PATH } from '../utils/constants';
-import ModalPortal from './ModalPortal';
-import ItemInformation from './ItemInformation';
 
 type itemProps = {
   props: restaurant;
@@ -14,46 +12,21 @@ type itemState = {
 };
 
 class Item extends React.Component<itemProps, itemState> {
-  constructor(props: itemProps | Readonly<itemProps>) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-
-    this.onClick = this.onClick.bind(this);
-    this.closeEvent = this.closeEvent.bind(this);
-  }
-
-  onClick() {
-    this.setState({ isOpen: true });
-  }
-
-  closeEvent() {
-    this.setState({ isOpen: false });
-  }
-
   render(): React.ReactNode {
     const { category, name, distance, description, id } = this.props.props;
     return (
-      <>
-        <li className="restaurant" data-id={id} onClick={this.onClick}>
-          <div className="restaurant_info">
-            <div className="restaurant__category">
-              <img src={CATEGORY_IMAGE_PATH[category]} alt={category} className="category-icon" />
-            </div>
-            <div className="restaurant__info">
-              <h3 className="restaurant__name text-subtitle">{name}</h3>
-              <span className="restaurant__distance text-body">캠퍼스로부터 {distance}분 내</span>
-              <p className="restaurant__description text-body">{description}</p>
-            </div>
+      <li className="restaurant" data-id={id}>
+        <div className="restaurant_info">
+          <div className="restaurant__category">
+            <img src={CATEGORY_IMAGE_PATH[category]} alt={category} className="category-icon" />
           </div>
-        </li>
-        {this.state.isOpen && (
-          <ModalPortal>
-            <ItemInformation restaurant={this.props.props} closeEvent={this.closeEvent} />
-          </ModalPortal>
-        )}
-      </>
+          <div className="restaurant__info">
+            <h3 className="restaurant__name text-subtitle">{name}</h3>
+            <span className="restaurant__distance text-body">캠퍼스로부터 {distance}분 내</span>
+            <p className="restaurant__description text-body">{description}</p>
+          </div>
+        </div>
+      </li>
     );
   }
 }
