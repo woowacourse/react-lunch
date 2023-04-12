f2d9d8aa0b58cc54c1788c84b2e682e33bd30963
ashleysyheo <ashleysyheo@gmail.com>
2023-04-12 16:15:45 +0900

    feat: FilterSection 컴포넌트 생성

---
+++ src/components/FilterSection.tsx
@@ -0,0 +1,53 @@
+import { Component, ReactNode } from 'react';
+import Select from './Select';
+import { RESTAURANT_CATEGORY_OPTION, SORT_BY } from '../constants';
+import { FilterSectionProps, FilterSectionState } from '../types';
+import { FilterOption } from '../types';
+
+class FilterSection extends Component<FilterSectionProps, FilterSectionState> {
+  state: FilterSectionState;
+
+  constructor(props: FilterSectionProps) {
+    super(props);
+
+    this.state = {
+      category: '전체',
+      sortBy: '이름순',
+    };
+  }
+
+  handleSelectChange(option: FilterOption) {
+    this.setState(
+      {
+        ...this.state,
+        ...option,
+      },
+      () => {
+        this.handleFilterChange();
+      }
+    );
+  }
+
+  handleFilterChange() {
+    this.props.onChange(this.state.category, this.state.sortBy);
+  }
+
+  render(): ReactNode {
+    return (
+      <div>
+        <Select
+          options={RESTAURANT_CATEGORY_OPTION}
+          attributes={{ id: 'category-filter', name: 'category', className: 'restaurant-filter' }}
+          onChange={(option: FilterOption) => this.handleSelectChange(option)}
+        />
+        <Select
+          options={SORT_BY}
+          attributes={{ id: 'sortBy-filter', name: 'sortBy', className: 'restaurant-filter' }}
+          onChange={(option: FilterOption) => this.handleSelectChange(option)}
+        />
+      </div>
+    );
+  }
+}
+
+export default FilterSection;
