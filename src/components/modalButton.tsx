55422277a52568c10cfd94eefb3cff0129f7acb5
NaveOWO <pianowell96@gmail.com>
2023-04-12 17:30:31 +0900

    feat : 버튼 컴포넌트 생성

---
+++ src/components/modalButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+export class ModalButton extends React.Component<{ text: string }> {
+  render() {
+    return <Button color={this.props.text}>{this.props.text}</Button>;
+  }
+}
+
+const Button = styled.button<{ color: string }>`
+  width: 100%;
+  height: 44px;
+
+  margin-right: 16px;
+
+  border: solid 1px
+    ${({ theme, color }) => (color === "닫기" ? "none" : theme.colors.grey500)};
+  border-radius: 8px;
+  color: ${({ theme, color }) =>
+    color === "닫기" ? theme.colors.grey100 : theme.colors.grey500};
+
+  font-weight: 600;
+  cursor: pointer;
+
+  ${({ theme }) => theme.fonts.caption}
+
+  background-color: ${({ theme, color }) =>
+    color === "닫기" ? theme.colors.primary : theme.colors.grey100};
+`;
