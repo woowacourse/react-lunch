4bffd8d23ad83fad1bea08afec62e5f36c32c4e7
woo-jk <201702042@o.cnu.ac.kr>
2023-04-12 18:38:31 +0900

    feat: App 컴포넌트 모달 관련 state 및 함수 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 import Filter from "./components/Filter";
 import Header from "./components/Header";
+import RestaurantDetailModal from "./components/RestaurantDetailModal";
 import RestaurantList from "./components/RestaurantList";
-import { CategoryOption, SortOption } from "./types/restaurant";
+import { CategoryOption, Restaurant, SortOption } from "./types/restaurant";
 
 interface AppState {
   selectedCategory: CategoryOption;
   selectedSort: SortOption;
+  isModalOpen: boolean;
+  modalContents: Restaurant;
 }
 class App extends Component {
   state: AppState = {
     selectedCategory: "all",
     selectedSort: "name",
+    isModalOpen: false,
+    modalContents: {
+      id: 0,
+      name: "",
+      category: "etc",
+      distance: 5,
+    },
   };
 
   setSelectedCategory(category: CategoryOption) {
@@ -23,7 +33,25 @@ class App extends Component {
     this.setState({ selectedSort: sort });
   }
 
+  setModalContents(restaurant: Restaurant) {
+    this.setState({ modalContents: restaurant });
+  }
+
+  openModal() {
+    this.setState({ isModalOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ isModalOpen: false });
+  }
+
   render() {
+    const ModalProps = {
+      isModalOpen: this.state.isModalOpen,
+      closeModal: this.closeModal.bind(this),
+      restaurant: this.state.modalContents,
+    };
+
     const filterProps = {
       setSelectedCategory: this.setSelectedCategory.bind(this),
       setSelectedSort: this.setSelectedSort.bind(this),
@@ -32,10 +60,13 @@ class App extends Component {
     const restaurantListProps = {
       selectedCategory: this.state.selectedCategory,
       selectedSort: this.state.selectedSort,
+      setModalContents: this.setModalContents.bind(this),
+      openModal: this.openModal.bind(this),
     };
 
     return (
       <div className="App">
+        <RestaurantDetailModal {...ModalProps} />
         <Header />
         <Filter {...filterProps} />
         <RestaurantList {...restaurantListProps} />
