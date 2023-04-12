ee9cb71dd6ef6113663db394eaba1572509ffd24
chex <chex1004@gmail.com>
2023-04-13 01:00:53 +0900

    refactor: App을 class 컴포넌트로 변경
    Co-authored-by: regularPark <srk99279@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import RestaurantDataService from './domains/RestaurantDataService';
 import RestaurantList from './components/RestaurantList/RestaurantList';
 
-function App() {
-  RestaurantDataService.setInitialRestaurants();
-  RestaurantDataService.getRestaurants();
-  return (
-    <div className="App">
-      <Header />
-      <RestaurantList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <RestaurantList />
+      </div>
+    );
+  }
 }
 
 export default App;
