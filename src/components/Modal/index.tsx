dc7aefabdf7d4ccb6a03448462635c95686eafb2
ashleysyheo <ashleysyheo@gmail.com>
2023-04-13 12:49:32 +0900

    feat: Modal 컴포넌트 폴더 생성

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,42 @@
+import { ModalProps } from '../../types';
+import './style.css';
+import { Component, MouseEvent } from 'react';
+
+class Modal extends Component<ModalProps> {
+  shouldComponentUpdate(nextProps: ModalProps) {
+    return this.props.isModalOpen !== nextProps.isModalOpen;
+  }
+
+  handleClose(event: MouseEvent<HTMLElement>) {
+    const target = event.target as HTMLElement;
+
+    if (
+      target.classList.contains('modal-backdrop') ||
+      target.classList.contains('modal-close-button')
+    ) {
+      this.props.onToggle();
+    }
+  }
+
+  render() {
+    const { isModalOpen } = this.props;
+
+    return (
+      <>
+        {isModalOpen && (
+          <div className="modal" onClick={(event) => this.handleClose(event)}>
+            <div className="modal-backdrop"></div>
+            <div className="modal-container">
+              {this.props.content}
+              <button className="button button--primary text-caption modal-close-button">
+                닫기
+              </button>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default Modal;
