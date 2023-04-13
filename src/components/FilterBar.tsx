93c99ca459a8da6d36fe94dd381c699e60e0fc2a
semnil5202 <semnil5202@gmail.com>
2023-04-13 15:07:11 +0900

    refactor: select 컴포넌트 분리
    Headless 패턴으로 외부에서 UI 주입
    
    Co-authored-by: ukkodeveloper <ukkodeveloper@users.noreply.github.com>

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,4 +1,5 @@
 import { PureComponent } from 'react';
+import Select from './Select';
 
 interface FilterBarProps {
   onChangeCategory: (event: React.ChangeEvent<HTMLSelectElement>) => void;
@@ -13,34 +14,39 @@ export default class FilterBar extends PureComponent<FilterBarProps> {
   render() {
     const { onChangeCategory, onChangeSort } = this.props;
     return (
-      <>
-        <section className="restaurant-filter-container">
-          <select
-            onChange={onChangeCategory}
-            name="category"
-            id="category-filter"
-            className="restaurant-filter"
-          >
-            <option value="전체">전체</option>
-            <option value="한식">한식</option>
-            <option value="중식">중식</option>
-            <option value="일식">일식</option>
-            <option value="양식">양식</option>
-            <option value="아시안">아시안</option>
-            <option value="기타">기타</option>
-          </select>
-
-          <select
-            onChange={onChangeSort}
-            name="sorting"
-            id="sorting-filter"
-            className="restaurant-filter"
-          >
-            <option value="name">이름순</option>
-            <option value="distance">거리순</option>
-          </select>
-        </section>
-      </>
+      <section className="restaurant-filter-container">
+        <Select>
+          {() => (
+            <select
+              onChange={onChangeCategory}
+              name="category"
+              id="category-filter"
+              className="restaurant-filter"
+            >
+              <option value="전체">전체</option>
+              <option value="한식">한식</option>
+              <option value="중식">중식</option>
+              <option value="일식">일식</option>
+              <option value="양식">양식</option>
+              <option value="아시안">아시안</option>
+              <option value="기타">기타</option>
+            </select>
+          )}
+        </Select>
+        <Select>
+          {() => (
+            <select
+              onChange={onChangeSort}
+              name="sorting"
+              id="sorting-filter"
+              className="restaurant-filter"
+            >
+              <option value="name">이름순</option>
+              <option value="distance">거리순</option>
+            </select>
+          )}
+        </Select>
+      </section>
     );
   }
 }
