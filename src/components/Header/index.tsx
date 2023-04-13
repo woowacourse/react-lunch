296099fa4ddb5bdc9b7e434c1c24280988c18ba8
ashleysyheo <ashleysyheo@gmail.com>
2023-04-13 12:45:25 +0900

    refactor: Header 컴포넌트 폴더 생성

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
+import './style.css';
 import { Component } from 'react';
 
 class Header extends Component {
   render() {
     return (
-      // ! HEADER 태그 추가함
       <header className="gnb">
         <a href="">
           <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
