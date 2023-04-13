c854fa9ab9efdb634c298b3a1caaf7505a227d47
hozzijeong <ghwls0905@naver.com>
2023-04-13 13:40:55 +0900

    feat: typeCheck 기능 구현

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,23 +1,3 @@
-import { parseJson } from './json';
-
-const localStorageGetItem = (key: string) => {
+export const localStorageGetItem = (key: string) => {
   return localStorage.getItem(key);
-};
-
-interface typeCheckProps<T> {
-  key: string;
-  initialValue?: T;
-}
-
-export const localStorageValueCheck = <T>({ key, initialValue }: typeCheckProps<T>): T => {
-  const value = localStorageGetItem(key);
-
-  if (value === null) {
-    if (initialValue !== undefined) return initialValue;
-    throw new Error('value is null');
-  }
-
-  const narrowedValue = parseJson<T>(value);
-
-  return narrowedValue;
 };
