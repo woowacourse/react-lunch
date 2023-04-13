4743a9dfc52d02816c5e5de5df48aa926bd488d6
유강현 <yrkdgus@gmail.com>
2023-04-13 21:58:40 +0900

    [점심 뭐 먹지 미션 Step 1] 쵸파(유강현) 미션 제출합니다. (#14)
    * chore: CRA로 초기 설정
    
    * docs: 기능 목록 작성
    
    * feat(Header): 헤더 컴포넌트 구현
    
    * feat(RestaurantList): 음식점 메인 화면 구현
    
    * refactor: 음식점 아이템을 컴포넌트로 추출
    
    * refactor: styled 컴포넌트 네이밍에 시맨틱 태그 명시
    
    * feat: 음식점 디테일 모달 템플릿 구현
    
    * feat: 음식점 타입 정의
    
    * feat: 음식점 아이템 컴포넌트에 props 적용
    
    * chore(img): 카테고리, 음식점 생성 버튼 이미지 추가
    
    * feat: 음식점 디테일 모달 컴포넌트에 props 적용
    
    * style: styled component 선언을 최하단으로 이동
    
    * feat: 음식점 상세 정보 모달 토글 기능 구현
    
    * feat: 음식점 목록의 필터링, 정렬 기능 구현
    
    * feat: 카테고리, 정렬 select box에 onChange이벤트 추가
    
    * refactor: 타입을 상수에서 추출할 수 있도록 변경
    
    * chore: 사용하지 않는 파일 및 주석 제거
    
    * chore: styled components 설치
    
    * chore: prettier 설정 추가
    
    * refactor: UI 및 마크업 수정
    
    * feat: local storage에 mock 데이터가 없으면 저장 및 데이터 불러오기 구현
    
    * chore: mock data 추가
    
    * refactor: class 선택자를 styled components로 변경
    
    * fixTypo: mockData 오타 수정
    
    * feat: 음식점 상세 닫기 UX 추가
    
    - 배경을 클릭해서 닫기
    - esc를 눌러서 닫기
    
    * style: 스타일 컴포넌트 선언을 클래스 아래로 이동
    
    * refactor: 폴더 구조 변경 및 사용하지 않는 파일 삭제
    
    * refactor: Button을 컴포넌트로 추출
    
    * refactor: Category 이미지를 컴포넌트로 추출
    
    * refactor: 컴포넌트 내보내기를 한 곳에 병합
    
    * chore: 서버에 실행에 필요한 파일 생성
    
    * chore: package.json에 homepage 주소 추가

---
+++ src/components/RestaurantDetail.tsx
@@ -0,0 +1,111 @@
+import { Restaurant } from '../types/restaurant';
+
+import React from 'react';
+import { Button, CategoryImage } from './';
+import styled from 'styled-components';
+
+import { textBody, textSubtitle } from '../style/mixin';
+
+interface Props {
+  closeModal: () => void;
+  restaurant: Restaurant;
+}
+
+class RestaurantDetail extends React.Component<Props> {
+  render() {
+    const { category, name, distance, description, link } = this.props.restaurant;
+
+    return (
+      <>
+        <ModalBackdrop onClick={this.props.closeModal} />
+        <ModalContainer>
+          <Detail>
+            <CategoryImage category={category} />
+            <RestaurantName>{name}</RestaurantName>
+            <Distance>캠퍼스부터 {distance}분 내</Distance>
+            <Description>{description}</Description>
+            <Link href={link}>{link}</Link>
+            <ButtonContainer>
+              <RemoveButton>삭제하기</RemoveButton>
+              <CloseButton onClick={this.props.closeModal}>닫기</CloseButton>
+            </ButtonContainer>
+          </Detail>
+        </ModalContainer>
+      </>
+    );
+  }
+}
+
+export default RestaurantDetail;
+
+const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+  padding: 32px 16px;
+
+  border-radius: 8px 8px 0px 0px;
+  background: var(--grey-100);
+`;
+
+const Detail = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  height: 508px;
+`;
+
+const RestaurantName = styled.h2`
+  ${textSubtitle}
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+const Distance = styled.p`
+  ${textBody}
+  color: #ec4a0a;
+`;
+
+const Description = styled.p`
+  height: 240px;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+`;
+
+const Link = styled.a`
+  height: 24px;
+  color: #000000;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+
+  button + button {
+    margin-left: 16px;
+  }
+`;
+
+const RemoveButton = styled(Button)`
+  border: 1px solid var(--grey-300);
+
+  background: transparent;
+  color: var(--grey-300);
+`;
+
+const CloseButton = styled(Button)``;
