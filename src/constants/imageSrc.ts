442cac484a959f8b1d0d7be3d28ce129b9cb2706
yogjin <youngjin988@gmail.com>
2023-04-13 17:24:11 +0900

    feat: imageSrc 타입 추가

--- src/constants/imageSrc.ts
+++ src/constants/imageSrc.ts
@@ -1,4 +1,6 @@
-export const imgSrc = {
+import { Category } from './../types';
+
+export const imgSrc: Record<Category, string> = {
   한식: `${process.env.PUBLIC_URL}/assets/category-korean.png`,
   중식: `${process.env.PUBLIC_URL}/assets/category-chinese.png`,
   일식: `${process.env.PUBLIC_URL}/assets/category-japanese.png`,
