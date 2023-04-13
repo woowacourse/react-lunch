731577ade802df0fe32d1b554b8fe417845716a3
Suyoung <jsy8492@gmail.com>
2023-04-13 13:12:44 +0900

    refactor: 음식점 상세 정보, 링크, 즐겨찾기 여부를 optional로 수정

--- src/domain/type.ts
+++ src/domain/type.ts
@@ -3,7 +3,7 @@ export interface Restaurant {
   category: string;
   name: string;
   distance: number;
-  description: string;
-  link: string;
-  isFavorite: boolean;
+  description?: string;
+  link?: string;
+  isFavorite?: boolean;
 }
