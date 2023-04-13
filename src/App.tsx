e311bf0c2ee1dd508ee66814315567090f0cce79
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-13 13:41:16 +0900

    refactor: GlobalStyle 태그 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import GlobalStyle from './style/global';
 import Header from './components/Header';
 import RestaurantList from './components/RestaurantList';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
+    <>
       <Header />
       <RestaurantList />
-    </div>
+    </>
   );
 }
 
