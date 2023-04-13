ba4daa6188191b1b6710b29f54efbd598f10c720
Gilpop8663 <wolfye@naver.com>
2023-04-13 14:20:31 +0900

    feat(types): 정렬방식 타입 추가 및 타입 가드 구현
    Co-authored-by: WaiNaat <qoli0703@gmail.com>

--- src/types/restaurantInfo.ts
+++ src/types/restaurantInfo.ts
@@ -1,4 +1,18 @@
-export type FoodCategory = '전체' | '일식' | '한식' | '중식' | '아시안' | '양식' | '기타';
+const foodCategory = ['전체', '일식', '한식', '중식', '아시안', '양식', '기타'] as const;
+
+const sortMethod = ['이름순', '거리순'] as const;
+
+export type FoodCategory = (typeof foodCategory)[number];
+
+export type SortMethod = (typeof sortMethod)[number];
+
+export const isFoodCategory = (category: string): category is FoodCategory => {
+  return foodCategory.includes(category as FoodCategory);
+};
+
+export const isSortMethod = (sortMethod: string): sortMethod is SortMethod => {
+  return sortMethod.includes(sortMethod as SortMethod);
+};
 
 export interface RestaurantInfo {
   title: string;
