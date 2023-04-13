54f7e5f1e833768b941354b59a55673aa13034eb
Dain Lee <feb.dain@gmail.com>
2023-04-13 16:07:58 +0900

    delete: 사용하지 않는 파일 삭제

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import "@testing-library/jest-dom/extend-expect";
-
-test("renders learn react link", async () => {
-  render(<App />);
-  const linkElement = screen.getByText("닫기");
-  expect(linkElement).toBeInTheDocument();
-});
