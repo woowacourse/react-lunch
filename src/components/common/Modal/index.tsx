8673f55dc2b76fff09e1a0d1773328f4f967b395
Suyoung <jsy8492@gmail.com>
2023-04-13 15:33:09 +0900

    feat: Modal을 ESC 키로 닫는 기능 구현

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -9,6 +9,20 @@ interface Props {
 }
 
 class Modal extends Component<Props> {
+  handleEscapeKeydown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  componentDidMount() {
+    document.body.addEventListener('keydown', this.handleEscapeKeydown);
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('keydown', this.handleEscapeKeydown);
+  }
+
   render() {
     return createPortal(
       <div role="dialog" aria-modal>
