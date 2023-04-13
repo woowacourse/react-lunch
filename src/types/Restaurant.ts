b7279cd4a534481d4edb4dd37da98222550b6db9
sh981013s <sh981013s@gmail.com>
2023-04-13 06:42:37 +0000

    feat: mockData 추가 및 id 프로퍼티 추가
    Co-authored-by: solo5star <solo_5@naver.com>

--- src/types/Restaurant.ts
+++ src/types/Restaurant.ts
@@ -2,6 +2,7 @@ import type { Category } from '../constants/categories';
 import type { DistancesByMinutes } from '../constants/distancesByMinutes';
 
 type Restaurant = {
+  id: number;
   name: string;
   category: Category;
   distanceByMinutes: DistancesByMinutes;
