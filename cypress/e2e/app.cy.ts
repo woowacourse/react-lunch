291c23f0456041984be3ac841b7a3d1f878d3b07
Suyoung <jsy8492@gmail.com>
2023-04-13 14:56:50 +0900

    refactor: mockData 샘플 데이터 추가 및 e2e 테스트 수정

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,8 +3,8 @@ describe('점심 뭐 먹지 앱 테스트', () => {
     cy.visit('http://localhost:3000/react-lunch');
   });
 
-  it('첫 페이지 접속 시 음식점 데이터 8개를 보여준다.', () => {
-    cy.get('.restaurant-list').children().should('have.length', 8);
+  it('첫 페이지 접속 시 음식점 데이터 18개를 보여준다.', () => {
+    cy.get('.restaurant-list').children().should('have.length', 18);
   });
 
   describe('카테고리별 필터링 기능', () => {
@@ -20,41 +20,7 @@ describe('점심 뭐 먹지 앱 테스트', () => {
     });
   });
 
-  describe('정렬 기능', () => {
-    context('거리순으로 정렬하면', () => {
-      it('거리가 가까운 순서대로 음식점이 보여야 한다.', () => {
-        cy.get('select[name="sorting"]').select('거리순');
-
-        cy.get('.restaurant-list').children().should('have.length', 8);
-        cy.get('.restaurant').eq(0).contains('도스타코스 선릉점');
-        cy.get('.restaurant').eq(1).contains('얌샘김밥');
-        cy.get('.restaurant').eq(2).contains('친친');
-        cy.get('.restaurant').eq(3).contains('잇쇼우');
-        cy.get('.restaurant').eq(4).contains('피양콩할마니');
-        cy.get('.restaurant').eq(5).contains('김돈이');
-        cy.get('.restaurant').eq(6).contains('호아빈 삼성점');
-        cy.get('.restaurant').eq(7).contains('이태리키친');
-      });
-    });
-
-    context('이름순으로 정렬하면', () => {
-      it('음식점 이름 오름차순으로 목록이 보여야 한다.', () => {
-        cy.get('select[name="sorting"]').select('이름순');
-
-        cy.get('.restaurant-list').children().should('have.length', 8);
-        cy.get('.restaurant').eq(0).contains('김돈이');
-        cy.get('.restaurant').eq(1).contains('도스타코스 선릉점');
-        cy.get('.restaurant').eq(2).contains('얌샘김밥');
-        cy.get('.restaurant').eq(3).contains('이태리키친');
-        cy.get('.restaurant').eq(4).contains('잇쇼우');
-        cy.get('.restaurant').eq(5).contains('친친');
-        cy.get('.restaurant').eq(6).contains('피양콩할마니');
-        cy.get('.restaurant').eq(7).contains('호아빈 삼성점');
-      });
-    });
-  });
-
-  describe('필터링 및 정렬 기능', () => {
+  describe('카테고리별 필터링 및 정렬 기능', () => {
     context('거리순으로 정렬하고 한식을 선택히면', () => {
       it('한식 카테고리의 음식점이 거리순으로 정렬된다.', () => {
         cy.get('select[name="sorting"]').select('거리순');
