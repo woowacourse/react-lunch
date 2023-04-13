64172020788987ca01d680dcfcb30fb61eac3dfb
NaveOWO <pianowell96@gmail.com>
2023-04-13 15:29:32 +0900

    feat : 모달 content 컴포넌트 생성

---
+++ src/components/ItemModalContent.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import { Restaurant } from "../types/restaurant";
+import { convertImage } from "../utils/imageConverter";
+
+interface PropsType {
+  restaurant: Restaurant;
+}
+
+export class ItemModalContent extends React.Component<PropsType> {
+  render() {
+    const { category, takingTime, name, link, description } =
+      this.props.restaurant;
+
+    return (
+      <>
+        <ModalIcon>
+          <img src={convertImage(category)} alt={category} />
+        </ModalIcon>
+        <Information>
+          <Name>{name}</Name>
+          <TakingTime>캠퍼스부터 {takingTime}분 내</TakingTime>
+          <Description>{description}</Description>
+          <Link target="_blank" href={link}>
+            {link}
+          </Link>
+        </Information>
+      </>
+    );
+  }
+}
+
+const ModalIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+  min-width: 64px;
+  min-height: 64px;
+
+  margin-right: 16px;
+
+  border-radius: 50%;
+  background: ${({ theme }) => theme.colors.lighten};
+`;
+
+const Information = styled.div`
+  height: 70%;
+`;
+
+const Name = styled.h3`
+  margin: 20px 0 16px 0;
+  ${({ theme }) => theme.fonts.subtitle}
+`;
+
+const TakingTime = styled.div`
+  color: ${({ theme }) => theme.colors.primary};
+  margin: 16px 0;
+  ${({ theme }) => theme.fonts.body};
+`;
+
+const Description = styled.p`
+  height: 260px;
+  margin-bottom: 16px;
+
+  overflow: scroll;
+  ${({ theme }) => theme.fonts.body};
+`;
+
+const Link = styled.a`
+  color: inherit;
+
+  ${({ theme }) => theme.fonts.body}
+  text-decoration: underline;
+`;
