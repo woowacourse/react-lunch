43aca9fe82a8bc306b0cc929a266de20018a9524
bassyu <yrkdgus@gmail.com>
2023-04-14 18:54:17 +0900

    fix(App): 새로고침을 해야 데이터가 반영되는 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Restaurant } from './types/restaurant';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Header, RestaurantList, RestaurantDetail } from './components';
 
 import mockData from './mockData.json';
 
 const App = () => {
+  if (!localStorage.getItem('restaurants')) {
+    localStorage.setItem('restaurants', JSON.stringify(mockData.restaurants));
+  }
+
   const restaurants = JSON.parse(localStorage.getItem('restaurants') || '[]') as Restaurant[];
-  const [detailId, setDetailId] = useState<Restaurant['id']>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [detailId, setDetailId] = useState<Restaurant['id']>('');
 
   const openModal = (id: Restaurant['id']) => {
     setDetailId(id);
@@ -19,20 +23,14 @@ const App = () => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    if (!localStorage.getItem('restaurants')) {
-      localStorage.setItem('restaurants', JSON.stringify(mockData.restaurants));
-    }
-  }, []);
-
   return (
     <div className="App">
       <Header />
       <RestaurantList restaurants={restaurants} openModal={openModal} />
       {isModalOpen && (
         <RestaurantDetail
-          closeModal={closeModal}
           restaurant={restaurants.find((restaurant) => restaurant.id === detailId) as Restaurant}
+          closeModal={closeModal}
         />
       )}
     </div>
