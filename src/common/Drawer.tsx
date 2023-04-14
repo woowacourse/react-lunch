c362b958bc28f4392abe2657addbac3edba1eb93
2yunseong <dbsdltjd3701@naver.com>
2023-04-15 00:04:44 +0900

    refactor(Drawer): 함수형 컴포넌트로 변경

--- src/common/Drawer.tsx
+++ src/common/Drawer.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
+
 type DrawerProps = {
   isOpenDrawer: boolean;
   children: React.ReactNode;
 };
 
-class Drawer extends React.Component<DrawerProps> {
-  render() {
-    return (
-      <div className={`modal ${this.props.isOpenDrawer && 'modal--open'}`}>
-        <div className="modal-backdrop"></div>
-        <div className="modal-container">{this.props.children}</div>
-      </div>
-    );
-  }
+const Drawer:React.FC<DrawerProps> = ({ isOpenDrawer, children }) => {
+  return (
+    <div className={`modal ${isOpenDrawer && 'modal--open'}`}>
+      <div className="modal-backdrop"></div>
+      <div className="modal-container">{children}</div>
+    </div>
+  );
 }
 
 export default Drawer;
