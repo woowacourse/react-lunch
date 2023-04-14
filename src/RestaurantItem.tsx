310aee7de95d7fe37671119fae3c0a449dbf1be0
2yunseong <dbsdltjd3701@naver.com>
2023-04-15 00:23:58 +0900

    refactor(RestaurantItem): 함수형 컴포넌트로 변경

--- src/RestaurantItem.tsx
+++ src/RestaurantItem.tsx
@@ -4,37 +4,36 @@ import { Restaurant } from './util/type';
 import { CATEGORY_IMAGES } from './util/constant.ts';
 
 type RestaurantProps = {
-  readonly restaurant: Omit<Restaurant, 'link'>;
+  restaurant: Omit<Restaurant, 'link'>;
   onToggleDrawer: (id?: number) => void;
 };
-class RestaurantItem extends React.Component<RestaurantProps> {
-  render() {
+const RestaurantItem:React.FC<RestaurantProps> = ({ restaurant, onToggleDrawer }) => {
+    const { id, category, title, distance, description } = restaurant;
     return (
       <li
         className="restaurant"
-        onClick={() => this.props.onToggleDrawer(this.props.restaurant.id)}
+        onClick={() => onToggleDrawer(id)}
       >
         <div className="restaurant__category">
           <img
-            src={CATEGORY_IMAGES[this.props.restaurant.category]}
-            alt={this.props.restaurant.category}
+            src={CATEGORY_IMAGES[category]}
+            alt={category}
             className="category-icon"
           />
         </div>
         <div className="restaurant__info">
           <h3 className="restaurant__name text-subtitle">
-            {this.props.restaurant.title}
+            {title}
           </h3>
           <span className="restaurant__distance text-body">
-            캠퍼스로부터 {this.props.restaurant.distance}분 내
+            캠퍼스로부터 {distance}분 내
           </span>
           <p className="restaurant__description text-body">
-            {this.props.restaurant.description}
+            {description}
           </p>
         </div>
       </li>
     );
-  }
 }
 
 export default RestaurantItem;
