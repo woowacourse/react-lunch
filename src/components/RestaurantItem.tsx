ce5897cb8e68e8d57dd0c7f3a43c0219d64b0bc5
cruelladevil <dev.timetravel@gmail.com>
2023-04-14 17:32:33 +0900

    refactor: RestaurantItem 함수형 컴포넌트로 수정

--- src/components/RestaurantItem.tsx
+++ src/components/RestaurantItem.tsx
@@ -1,74 +1,36 @@
-import React from "react";
-import { CATEGORY_ICONS } from "../constants/constants";
-import Modal from "./Modal";
-import type { Restaurant } from "../types/Restaurant";
+import { useState } from 'react';
+import { CATEGORY_ICONS } from '../constants/constants';
+import Modal from './Modal';
+import type { Restaurant } from '../types/Restaurant';
 
 type RestaurantItemProps = {
   restaurant: Restaurant;
 };
 
-type RestaurantItemState = {
-  isModalOpen: boolean;
-};
+function RestaurantItem({ restaurant }: RestaurantItemProps) {
+  const { storeName, category, distance, detail } = restaurant;
 
-class RestaurantItem extends React.Component<
-  RestaurantItemProps,
-  RestaurantItemState
-> {
-  constructor(props: RestaurantItemProps) {
-    super(props);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    this.state = {
-      isModalOpen: false,
-    };
+  const showModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
-    this.showModal = this.showModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
+  return (
+    <>
+      <li className="restaurant" onClick={showModal}>
+        <div className="restaurant__category">
+          <img src={CATEGORY_ICONS[category]} alt={category} className="category-icon" />
+        </div>
+        <div className="restaurant__info">
+          <h3 className="restaurant__name text-subtitle">{storeName}</h3>
+          <span className="restaurant__distance text-body">캠퍼스부터 {distance}분 내</span>
+          <p className="restaurant__description text-body">{detail}</p>
+        </div>
+      </li>
 
-  render() {
-    const { storeName, category, distance, detail } = this.props.restaurant;
-
-    return (
-      <>
-        <li className="restaurant" onClick={this.showModal}>
-          <div className="restaurant__category">
-            <img
-              src={CATEGORY_ICONS[category]}
-              alt={category}
-              className="category-icon"
-            />
-          </div>
-          <div className="restaurant__info">
-            <h3 className="restaurant__name text-subtitle">{storeName}</h3>
-            <span className="restaurant__distance text-body">
-              캠퍼스부터 {distance}분 내
-            </span>
-            <p className="restaurant__description text-body">{detail}</p>
-          </div>
-        </li>
-
-        {this.state.isModalOpen && (
-          <Modal
-            restaurant={this.props.restaurant}
-            closeModal={this.closeModal}
-          />
-        )}
-      </>
-    );
-  }
-
-  showModal() {
-    this.setState({
-      isModalOpen: true,
-    });
-  }
-
-  closeModal() {
-    this.setState({
-      isModalOpen: false,
-    });
-  }
+      {isModalOpen && <Modal restaurant={restaurant} closeModal={closeModal} />}
+    </>
+  );
 }
 
 export default RestaurantItem;
