ee084498513d86029561b368c5caf15f36bbf4ee
sh981013s <sh981013s@gmail.com>
2023-04-14 21:35:07 +0900

    refactor: RestaurantList 컴포넌트 함수형 컴포넌트로 리팩토링

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -7,20 +7,18 @@ type RestaurantListProps = {
   restaurants: Restaurant[];
   onClickItem: (restaurant: Restaurant) => void;
 };
-class RestaurantList extends React.PureComponent<RestaurantListProps> {
-  render() {
-    return (
-      <styled.RestaurantList>
-        {this.props.restaurants.map((restaurant) => (
-          <RestaurantListItem
-            key={restaurant.id}
-            restaurant={restaurant}
-            onClick={() => this.props.onClickItem(restaurant)}
-          />
-        ))}
-      </styled.RestaurantList>
-    );
-  }
-}
+const RestaurantList = (props: RestaurantListProps) => {
+  return (
+    <styled.RestaurantList>
+      {props.restaurants.map((restaurant) => (
+        <RestaurantListItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          onClick={() => props.onClickItem(restaurant)}
+        />
+      ))}
+    </styled.RestaurantList>
+  );
+};
 
 export default RestaurantList;
