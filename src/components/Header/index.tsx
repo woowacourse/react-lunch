af5f234dc47fb8cca3aa1c574d0f15496708f8b3
ashleysyheo <ashleysyheo@gmail.com>
2023-04-14 10:53:17 +0900

    refactor: RestaurantItem 컴포넌트 함수형 컴포넌트로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,13 @@
 import './style.css';
-import { Component } from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="gnb">
-        <a href="">
-          <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-        </a>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="gnb">
+      <a href="">
+        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+      </a>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/RestaurantItem/index.tsx
+++ src/components/RestaurantItem/index.tsx
@@ -1,5 +1,4 @@
 import './style.css';
-import { Component } from 'react';
 import { Restaurant } from '../../types';
 import { RESTAURANT_IMAGE } from '../../constants/images';
 
@@ -7,31 +6,27 @@ interface RestaurantItemProps {
   restaurant: Restaurant;
 }
 
-class RestaurantItem extends Component<RestaurantItemProps> {
-  render() {
-    const { restaurant } = this.props;
-
-    return (
-      <li className="restaurant" data-id={restaurant.id}>
-        <div className="restaurant__category">
-          <img
-            src={RESTAURANT_IMAGE[restaurant.category]}
-            alt={restaurant.category}
-            className="category-icon"
-          />
+function RestaurantItem({ restaurant }: RestaurantItemProps) {
+  return (
+    <li className="restaurant" data-id={restaurant.id}>
+      <div className="restaurant__category">
+        <img
+          src={RESTAURANT_IMAGE[restaurant.category]}
+          alt={restaurant.category}
+          className="category-icon"
+        />
+      </div>
+      <div className="restaurant__info-container">
+        <div className="restaurant__info">
+          <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
+          <span className="restaurant__distance text-body">
+            캠퍼스부터 {restaurant.distance}분 내
+          </span>
+          <p className="restaurant__description text-body">{restaurant.description ?? ''}</p>
         </div>
-        <div className="restaurant__info-container">
-          <div className="restaurant__info">
-            <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
-            <span className="restaurant__distance text-body">
-              캠퍼스부터 {restaurant.distance}분 내
-            </span>
-            <p className="restaurant__description text-body">{restaurant.description ?? ''}</p>
-          </div>
-        </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
 }
 
 export default RestaurantItem;
