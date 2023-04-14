86a387b98a692c1244d0865d8322e24dd0dffc45
2yunseong <dbsdltjd3701@naver.com>
2023-04-15 00:46:38 +0900

    refactor(RestaurantList): 함수형 컴포넌트로 변경

--- src/RestaurantList.tsx
+++ src/RestaurantList.tsx
@@ -1,47 +1,42 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import RestaurantItem from './RestaurantItem.tsx';
 import { FilterOption, Restaurant } from './util/type.js';
 import { pipe } from './util/util.ts';
-type RestaurantListState = {
-  restaurantList: Omit<Restaurant, 'link'>[];
-};
+
+type RestaurantListState = Omit<Restaurant, 'link'>[];
 
 type RestaurantListProps = {
   filterOptions: FilterOption;
   onToggleDrawer: (id?: number) => void;
 };
 
-class RestaurantList extends React.Component<RestaurantListProps, RestaurantListState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      restaurantList: [],
-    };
-  }
+const RestaurantList:React.FC<RestaurantListProps> = ({ filterOptions, onToggleDrawer }) => {
+  const { category, sorting } = filterOptions;
+  // TODO: 타입 구체화
+  const [restaurantList, setRestaurantList] = useState<RestaurantListState>([]);
 
-  componentDidMount(): void {
+  useEffect(() => {
     const rawRestaurantList = localStorage.getItem('restaurantList');
-    if (rawRestaurantList) {
-      this.setState({ restaurantList: JSON.parse(rawRestaurantList) });
-      return;
-    }
-
-    fetch('./mockData.json')
+    if (rawRestaurantList) { 
+        setRestaurantList(JSON.parse(rawRestaurantList));
+    } else {
+      fetch('./mockData.json')
       .then((res) => res.json())
       .then((data) => {
         localStorage.setItem('restaurantList', JSON.stringify(data));
-        this.setState({ restaurantList: data });
+        setRestaurantList(data);
       });
-  }
-
-  filterByCategory(restaurantList, category): Restaurant[] {
+    }
+  }, []);
+  
+  const filterByCategory = (restaurantList, category):Restaurant[] =>  {
     if (category === '전체') return restaurantList;
     return restaurantList.filter(
       (restaurant) => restaurant.category === category
     );
   }
 
-  filterBySort(restaurantList, sorting): Restaurant[] {
+  const filterBySort = (restaurantList, sorting):Restaurant[] => {
     return restaurantList.sort((firstElement, secondElement) => {
       if (sorting === 'name') {
         return firstElement.title.localeCompare(secondElement.title);
@@ -53,30 +48,22 @@ class RestaurantList extends React.Component<RestaurantListProps, RestaurantList
     });
   }
 
-  sortRestaurants(rl, category, sorting) {
-    return this.filterBySort(this.filterByCategory(rl, category), sorting);
-  }
-
-  render() {
-    const { category, sorting } = this.props.filterOptions;
-
     return (
       <section className="restaurant-list-container">
         <ul className="restaurant-list">
-          {pipe(this.filterByCategory, this.filterBySort)(
-            this.state.restaurantList,
+          {pipe(filterByCategory, filterBySort)(
+            restaurantList,
             [category, sorting]
           ).map((restaurant) => (
             <RestaurantItem
               key={restaurant.id}
               restaurant={restaurant}
-              onToggleDrawer={this.props.onToggleDrawer}
+              onToggleDrawer={onToggleDrawer}
             />
           ))}
         </ul>
       </section>
     );
-  }
 }
 
 export default RestaurantList;
