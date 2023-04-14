70a3b5315fb53a38c5b102ad5716b47f71eca9e5
ashleysyheo <ashleysyheo@gmail.com>
2023-04-14 10:54:29 +0900

    refactor: RestaurantDetail 컴포넌트 함수형 컴포넌트로 변경

--- src/components/RestaurantDetail/index.tsx
+++ src/components/RestaurantDetail/index.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Restaurant } from '../../types';
 import { RESTAURANT_IMAGE } from '../../constants/images';
 
@@ -6,42 +5,32 @@ interface RestaurantDetailProps {
   restaurant: Restaurant;
 }
 
-class RestaurantDetail extends Component<RestaurantDetailProps> {
-  render() {
-    const { restaurant } = this.props;
-
-    return (
-      <>
-        {restaurant && (
-          <>
-            <div className="restaurant__information">
-              <div className="restaurant__category">
-                <img
-                  src={RESTAURANT_IMAGE[restaurant.category]}
-                  alt={restaurant.category}
-                  className="category-icon"
-                />
-              </div>
-              <div className="restaurant-detail__info-text">
-                <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
-                <span className="restaurant__distance text-body">
-                  캠퍼스부터 {restaurant.distance}분 내
-                </span>
-              </div>
-            </div>
-            <p className="restaurant-detail__description text-body">
-              {restaurant.description ?? ''}
-            </p>
-            {restaurant.link && (
-              <a href={restaurant.link} className="restaurant__link" target="_blank">
-                웹사이트 방문하기
-              </a>
-            )}
-          </>
-        )}
-      </>
-    );
-  }
+function RestaurantDetail({ restaurant }: RestaurantDetailProps) {
+  return (
+    <>
+      <div className="restaurant__information">
+        <div className="restaurant__category">
+          <img
+            src={RESTAURANT_IMAGE[restaurant.category]}
+            alt={restaurant.category}
+            className="category-icon"
+          />
+        </div>
+        <div className="restaurant-detail__info-text">
+          <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
+          <span className="restaurant__distance text-body">
+            캠퍼스부터 {restaurant.distance}분 내
+          </span>
+        </div>
+      </div>
+      <p className="restaurant-detail__description text-body">{restaurant.description ?? ''}</p>
+      {restaurant.link && (
+        <a href={restaurant.link} className="restaurant__link" target="_blank">
+          웹사이트 방문하기
+        </a>
+      )}
+    </>
+  );
 }
 
 export default RestaurantDetail;
