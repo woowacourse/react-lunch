21e3132266b7a92bf9c0e576c01c2f8ae286e64b
bassyu <yrkdgus@gmail.com>
2023-04-14 16:50:06 +0900

    refactor(App): 함수형 컴포넌트로 마이그레이션

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,42 @@
-import { Category, Restaurant, SortingType } from './types/restaurant';
+import { Restaurant } from './types/restaurant';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header, RestaurantList, RestaurantDetail } from './components';
 
 import mockData from './mockData.json';
 
-interface Props {}
+const App = () => {
+  const restaurants = JSON.parse(localStorage.getItem('restaurants') || '[]') as Restaurant[];
+  const [detailId, setDetailId] = useState<Restaurant['id']>('');
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-interface State {
-  restaurants: Restaurant[];
-  isModalOpen: boolean;
-  detailId: Restaurant['id'];
-}
+  const openModal = (id: Restaurant['id']) => {
+    setDetailId(id);
+    setIsModalOpen(true);
+  };
 
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
+  useEffect(() => {
     if (!localStorage.getItem('restaurants')) {
       localStorage.setItem('restaurants', JSON.stringify(mockData.restaurants));
     }
+  }, []);
 
-    const restaurants = JSON.parse(localStorage.getItem('restaurants') || '[]');
-    this.state = {
-      restaurants,
-      isModalOpen: false,
-      detailId: '1'
-    };
-
-    window.addEventListener('keyup', ({ key }) => {
-      if (this.state.isModalOpen && key === 'Escape') {
-        this.closeModal();
-      }
-    });
-  }
-
-  openModal = (id: Restaurant['id']) => {
-    this.setState({
-      detailId: id,
-      isModalOpen: true
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      isModalOpen: false
-    });
-  };
-
-  render() {
-    const { restaurants, detailId } = this.state;
-    return (
-      <div className="App">
-        <Header />
-        <RestaurantList restaurants={restaurants} openModal={this.openModal} />
-        {this.state.isModalOpen && (
-          <RestaurantDetail
-            closeModal={this.closeModal}
-            restaurant={restaurants.find((restaurant) => restaurant.id === detailId) as Restaurant}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Header />
+      <RestaurantList restaurants={restaurants} openModal={openModal} />
+      {isModalOpen && (
+        <RestaurantDetail
+          closeModal={closeModal}
+          restaurant={restaurants.find((restaurant) => restaurant.id === detailId) as Restaurant}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
