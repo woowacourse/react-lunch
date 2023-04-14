1996c275afe55d8e3b172242254d54acd57fd4c7
cruelladevil <dev.timetravel@gmail.com>
2023-04-14 17:52:54 +0900

    refactor: Select 함수형 컴포넌트로 수정

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,7 +1,6 @@
-import React, { ChangeEvent } from "react";
-import { CATEGORIES, SORT_BY } from "../constants/constants";
-
-import type { Category, SortBy } from "../types/Restaurant";
+import { ChangeEvent } from 'react';
+import { CATEGORIES, SORT_BY } from '../constants/constants';
+import type { Category, SortBy } from '../types/Restaurant';
 
 type SelectProps = {
   setCategory: (newCategory: Category) => void;
@@ -22,49 +21,42 @@ const isSortBy = (sortBy: string): sortBy is SortBy => {
   return false;
 };
 
-class Select extends React.Component<SelectProps> {
-  onChangeCategory(event: ChangeEvent<HTMLSelectElement>) {
-    if (isCategory(event.target.value)) {
-      this.props.setCategory(event.target.value);
-    }
-  }
+function Select({ setCategory, setSort }: SelectProps) {
+  const onChangeCategory = (event: ChangeEvent<HTMLSelectElement>) => {
+    if (isCategory(event.target.value)) setCategory(event.target.value);
+  };
+  const onChangeSort = (event: ChangeEvent<HTMLSelectElement>) => {
+    if (isSortBy(event.target.value)) setSort(event.target.value);
+  };
 
-  onChangeSort(event: ChangeEvent<HTMLSelectElement>) {
-    if (isSortBy(event.target.value)) {
-      this.props.setSort(event.target.value);
-    }
-  }
+  return (
+    <section className="restaurant-filter-container">
+      <select
+        name="category"
+        id="category-filter"
+        className="restaurant-filter"
+        onChange={(event) => onChangeCategory(event)}
+      >
+        <option value="전체">전체</option>
+        <option value="한식">한식</option>
+        <option value="중식">중식</option>
+        <option value="일식">일식</option>
+        <option value="양식">양식</option>
+        <option value="아시안">아시안</option>
+        <option value="기타">기타</option>
+      </select>
 
-  render() {
-    return (
-      <section className="restaurant-filter-container">
-        <select
-          name="category"
-          id="category-filter"
-          className="restaurant-filter"
-          onChange={(event) => this.onChangeCategory(event)}
-        >
-          <option value="전체">전체</option>
-          <option value="한식">한식</option>
-          <option value="중식">중식</option>
-          <option value="일식">일식</option>
-          <option value="양식">양식</option>
-          <option value="아시안">아시안</option>
-          <option value="기타">기타</option>
-        </select>
-
-        <select
-          name="sorting"
-          id="sorting-filter"
-          className="restaurant-filter"
-          onChange={(event) => this.onChangeSort(event)}
-        >
-          <option value="이름순">이름순</option>
-          <option value="거리순">거리순</option>
-        </select>
-      </section>
-    );
-  }
+      <select
+        name="sorting"
+        id="sorting-filter"
+        className="restaurant-filter"
+        onChange={(event) => onChangeSort(event)}
+      >
+        <option value="이름순">이름순</option>
+        <option value="거리순">거리순</option>
+      </select>
+    </section>
+  );
 }
 
 export default Select;
