155b0a265026d754e9b09fbcdb0e120892437807
ashleysyheo <ashleysyheo@gmail.com>
2023-04-14 09:54:25 +0900

    fix: 파일 경로 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Header from './components/Header';
-import Main from './components/Restaurant';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
