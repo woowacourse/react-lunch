f30cb948497bd710465418f56b32e738c61f783f
sh981013s <sh981013s@gmail.com>
2023-04-14 15:09:24 +0900

    refactor: ButtonProps 간소화

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import * as styled from './Button.styles';
 
-type ButtonProps = Omit<
-  React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
-  'ref'
-> &
-  React.PropsWithChildren<{
-    variant?: 'primary' | 'outlined';
-  }>;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  React.PropsWithChildren<{ variant?: 'primary' | 'outlined' }>;
 
 class Button extends React.PureComponent<ButtonProps> {
   render() {
