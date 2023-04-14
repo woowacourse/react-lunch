d3c9be02882b3094278a368afdc24015e4b09ada
cruelladevil <dev.timetravel@gmail.com>
2023-04-14 17:49:23 +0900

    refactor: Modal 함수형 컴포넌트로 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,78 +1,52 @@
-import React from "react";
-import { createPortal } from "react-dom";
-import { CATEGORY_ICONS } from "../constants/constants";
-import type { Restaurant } from "../types/Restaurant";
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { CATEGORY_ICONS } from '../constants/constants';
+import type { Restaurant } from '../types/Restaurant';
 
-type RestaurantItemProps = {
+type ModalProps = {
   restaurant: Restaurant;
   closeModal: () => void;
 };
 
-class Modal extends React.Component<RestaurantItemProps> {
-  constructor(props: RestaurantItemProps) {
-    super(props);
-    this.closeModalByESC = this.closeModalByESC.bind(this);
-    this.closeModalByBackdrop = this.closeModalByBackdrop.bind(this);
-  }
+function Modal({ restaurant, closeModal }: ModalProps) {
+  const { category, storeName, distance, detail, link } = restaurant;
 
-  closeModalByBackdrop() {
-    this.props.closeModal();
-  }
+  const closeModalByESC = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') closeModal();
+  };
 
-  closeModalByESC(event: KeyboardEvent) {
-    if (event.key === "Escape") this.props.closeModal();
-  }
+  useEffect(() => {
+    document.addEventListener('keydown', closeModalByESC);
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.closeModalByESC);
-  }
+    return () => document.removeEventListener('keydown', closeModalByESC);
+  });
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.closeModalByESC);
-  }
-
-  render() {
-    return createPortal(
-      <>
-        <div
-          className="modal-backdrop"
-          onClick={this.closeModalByBackdrop}
-        ></div>
-        <div className="modal-container">
-          <div className="information-modal-favorite">
-            <div className="restaurant__category">
-              <img
-                src={CATEGORY_ICONS[this.props.restaurant.category]}
-                alt={this.props.restaurant.category}
-                className="category-icon"
-              />
-            </div>
-          </div>
-          <div className="restaurant__name text-subtitle" id="storeName">
-            {this.props.restaurant.storeName}
-          </div>
-          <div className="restaurant__distance text-body">
-            캠퍼스부터 {this.props.restaurant.distance}분 내
-          </div>
-          <div className="restaurant__description text-body">
-            {this.props.restaurant.detail}
-          </div>
-          <a href={this.props.restaurant.link} className="restaurant-link">
-            {this.props.restaurant.link}
-          </a>
-          <div className="button-container">
-            <button
-              onClick={this.props.closeModal}
-              className="button button--primary text-caption"
-            >
-              닫기
-            </button>
+  return createPortal(
+    <>
+      <div className="modal-backdrop" onClick={closeModal}></div>
+      <div className="modal-container">
+        <div className="information-modal-favorite">
+          <div className="restaurant__category">
+            <img src={CATEGORY_ICONS[category]} alt={category} className="category-icon" />
           </div>
         </div>
-      </>,
-      document.body
-    );
-  }
+        <div className="restaurant__name text-subtitle" id="storeName">
+          {storeName}
+        </div>
+        <div className="restaurant__distance text-body">캠퍼스부터 {distance}분 내</div>
+        <div className="restaurant__description text-body">{detail}</div>
+        <a href={link} className="restaurant-link">
+          {link}
+        </a>
+        <div className="button-container">
+          <button onClick={closeModal} className="button button--primary text-caption">
+            닫기
+          </button>
+        </div>
+      </div>
+    </>,
+    document.body,
+  );
 }
 
 export default Modal;
