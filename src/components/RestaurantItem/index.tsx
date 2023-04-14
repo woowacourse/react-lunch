7fb08729caffc313decbfe68fb8d3ec448855997
minjaeKim <smallkdb@gmail.com>
2023-04-14 16:11:57 +0900

    refactor: 아이콘 주소를 전달하는 함수 utils로 분리

--- src/components/RestaurantItem/index.tsx
+++ src/components/RestaurantItem/index.tsx
@@ -1,35 +1,13 @@
 import React, { useContext } from 'react';
-import categoryAsian from '../../assets/category-asian.png';
-import categoryChinese from '../../assets/category-chinese.png';
-import categoryEtc from '../../assets/category-etc.png';
-import categoryJapanese from '../../assets/category-japanese.png';
-import categoryKorean from '../../assets/category-korean.png';
-import categoryWestern from '../../assets/category-western.png';
 import Store from '../../store';
+import pickIconByCategory from '../../utils/pickIconByCategory';
 import styles from './RestaurantItem.module.css';
-import type { Category, Restaurant } from './type';
+import type { Restaurant } from './type';
 
 interface Props {
 	restaurant: Restaurant;
 	isModal: boolean;
 }
-
-const makeCategoryImgPath = (category: Category) => {
-	switch (category) {
-		case '한식':
-			return categoryKorean;
-		case '일식':
-			return categoryJapanese;
-		case '중식':
-			return categoryChinese;
-		case '아시안':
-			return categoryAsian;
-		case '양식':
-			return categoryWestern;
-		default:
-			return categoryEtc;
-	}
-};
 
 function RestaurantItem({ restaurant, isModal }: Props) {
 	const { setModalId, setIsModalOpen } = useContext(Store);
@@ -44,7 +22,7 @@ function RestaurantItem({ restaurant, isModal }: Props) {
 	return (
 		<li className={isModal ? styles.itemModal : styles.item} onClick={openModal}>
 			<div className={styles.icon}>
-				<img src={makeCategoryImgPath(category)} alt={category} />
+				<img src={pickIconByCategory(category)} alt={category} />
 			</div>
 			<article className={isModal ? styles.articleModal : undefined}>
 				<h2 className={styles.title}>{name}</h2>
