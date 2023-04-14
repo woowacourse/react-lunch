b7f2109c93be9dba1302da5b7f9fcbe8a5168d3a
2yunseong <dbsdltjd3701@naver.com>
2023-04-15 00:08:29 +0900

    refactor(Select): 함수형 컴포넌트로 변경

--- src/common/Select.tsx
+++ src/common/Select.tsx
@@ -8,23 +8,21 @@ type SelectProps = {
   onChangeFilterOptions: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
-class Select extends React.Component<SelectProps> {
-  render() {
-    return (
-      <select
-        name={this.props.name}
-        id="category-filter"
-        className="restaurant-filter"
-        onChange={this.props.onChangeFilterOptions}
-      >
-        {this.props.options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.textContent}
-          </option>
-        ))}
-      </select>
-    );
-  }
-}
-
+const Select:React.FC<SelectProps> = ({ name, options, onChangeFilterOptions }) => {
+  return (
+    <select
+      name={name}
+      id="category-filter"
+      className="restaurant-filter"
+      onChange={onChangeFilterOptions}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.textContent}
+        </option>
+      ))}
+    </select>
+  );
+};
+  
 export default Select;
