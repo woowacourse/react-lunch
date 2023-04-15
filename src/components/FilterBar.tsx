0f42edcfa0485cfdbd673603d5f84dc59e6a14e6
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-15 16:54:34 +0900

    refactor: Select 컴포넌트 삭제 및 options 배열순회하도록 변경

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,5 +1,5 @@
 import { PureComponent } from 'react';
-import Select from './Select';
+
 import {
   RESTAURANT_CATEGORY,
   SORTING_OPTION,
@@ -19,37 +19,27 @@ export default class FilterBar extends PureComponent<FilterBarProps> {
     const { onChangeCategory, onChangeSort } = this.props;
     return (
       <section className="restaurant-filter-container">
-        <Select>
-          {() => (
-            <select
-              onChange={onChangeCategory}
-              name="category"
-              id="category-filter"
-              className="restaurant-filter"
-            >
-              <option value="전체">{RESTAURANT_CATEGORY.all}</option>
-              <option value="한식">{RESTAURANT_CATEGORY.korea}</option>
-              <option value="중식">{RESTAURANT_CATEGORY.chinese}</option>
-              <option value="일식">{RESTAURANT_CATEGORY.japanese}</option>
-              <option value="양식">{RESTAURANT_CATEGORY.western}</option>
-              <option value="아시안">{RESTAURANT_CATEGORY.asian}</option>
-              <option value="기타">{RESTAURANT_CATEGORY.etc}</option>
-            </select>
-          )}
-        </Select>
-        <Select>
-          {() => (
-            <select
-              onChange={onChangeSort}
-              name="sorting"
-              id="sorting-filter"
-              className="restaurant-filter"
-            >
-              <option value="name">{SORTING_OPTION.name}</option>
-              <option value="distance">{SORTING_OPTION.distance}</option>
-            </select>
-          )}
-        </Select>
+        <select
+          onChange={onChangeCategory}
+          name="category"
+          id="category-filter"
+          className="restaurant-filter"
+        >
+          {Object.values(RESTAURANT_CATEGORY).map((category) => (
+            <option value={category}>{category}</option>
+          ))}
+        </select>
+
+        <select
+          onChange={onChangeSort}
+          name="sorting"
+          id="sorting-filter"
+          className="restaurant-filter"
+        >
+          {Object.values(SORTING_OPTION).map((sortingName) => (
+            <option value={sortingName}>{sortingName}</option>
+          ))}
+        </select>
       </section>
     );
   }
