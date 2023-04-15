453d71cd12446b14b1a3912129990a390e1bf28b
yeopto <gunyeop530@gmail.com>
2023-04-15 15:06:33 +0900

    refactor: Filter 컴포넌트를 함수형 컴포넌트로 변경하다

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,23 +1,22 @@
-import { Component } from 'react';
 import { FilterProps } from '../../types/types';
 import styles from './Filter.module.css';
 
-export default class Filter extends Component<FilterProps> {
-  render() {
-    const { name, options, onChange } = this.props;
+const Filter = (props: FilterProps) => {
+  const { name, options, onChange } = props;
 
-    const optionElements = options.map(option => {
-      return (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      );
-    });
-
+  const optionElements = options.map(option => {
     return (
-      <select className={styles.filter} name={name} onChange={onChange}>
-        {optionElements}
-      </select>
+      <option key={option.value} value={option.value}>
+        {option.label}
+      </option>
     );
-  }
-}
+  });
+
+  return (
+    <select className={styles.filter} name={name} onChange={onChange}>
+      {optionElements}
+    </select>
+  );
+};
+
+export default Filter;
