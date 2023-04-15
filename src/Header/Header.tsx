301cdd3078b32ec3a6b2c9c4dbd4d8a7be648b4a
gyeongza <gyeongza@naver.com>
2023-04-16 01:22:36 +0900

    [step2] refactor: Header 컴포넌트 함수형으로 변경

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <header id="head" className="gnb">
-        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header id="head" className="gnb">
+      <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+    </header>
+  );
 }
 
 export default Header;
