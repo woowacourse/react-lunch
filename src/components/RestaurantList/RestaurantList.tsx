45566e5acb8cc064cf72210596b2c7705e15adec
regularPark <srk99279@naver.com>
2023-04-15 19:30:48 +0900

    refactor: RestaurantList 훅 변경

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,19 +1,44 @@
-import { Restaurant } from '../../types';
+import { useState } from 'react';
+import { Restaurant, Category, All, Criterion } from '../../types';
 import RestaurantItem from '../RestaurantItem/RestaurantItem';
 import DetailModal from '../Modal/DetailModal';
 import CategoryFilter from '../SelectBox/CategoryFilter';
 import SortingFilter from '../SelectBox/SortingFilter';
-import useRestaurantList from './useRestaurantList';
+import LunchDataService from '../../domains/LunchDataService';
 
 function RestaurantList() {
-  const {
-    isClicked,
-    clickedData,
-    restaurantsData,
-    handleSetCategory,
-    handleSetCriterion,
-    onClick,
-  } = useRestaurantList();
+  const [isItemClicked, setIsItemClicked] = useState<boolean>(false);
+  const [clickedData, setClickedData] = useState<Restaurant>({
+    id: 0,
+    category: '한식',
+    name: '',
+    distance: 0,
+    description: '',
+    link: '',
+  });
+  const [category, setCategory] = useState<Category | All>('전체');
+  const [criterion, setCriterion] = useState<Criterion>('이름순');
+  const [restaurantsData, setRestaurantsData] = useState<Restaurant[]>(
+    LunchDataService.getRestaurants('전체', '이름순'),
+  );
+
+  const onClick = (targetId: string) => {
+    if (!targetId) return;
+    setIsItemClicked(true);
+    setClickedData(LunchDataService.getRestaurant(targetId));
+  };
+
+  const handleSetCategory = (newCategory: Category | All) => {
+    setIsItemClicked(false);
+    setCategory(newCategory);
+    setRestaurantsData(LunchDataService.getRestaurants(newCategory, criterion));
+  };
+
+  const handleSetCriterion = (newCriterion: Criterion) => {
+    setIsItemClicked(false);
+    setCriterion(newCriterion);
+    setRestaurantsData(LunchDataService.getRestaurants(category, newCriterion));
+  };
 
   return (
     <>
@@ -26,7 +51,7 @@ function RestaurantList() {
           return <RestaurantItem key={restaurantData.id} data={restaurantData} onClick={onClick} />;
         })}
       </ul>
-      {isClicked && <DetailModal data={clickedData} />}
+      {isItemClicked && <DetailModal data={clickedData} isClicked={isItemClicked} />}
     </>
   );
 }
