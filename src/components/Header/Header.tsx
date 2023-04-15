13363c04711bcf6b8061846b842733ee8e9afd64
jeongwusi <jeongwusi@gmail.com>
2023-04-16 07:49:12 +0900

    ♻️ refactor: Header 컴포넌트를 함수형으로 변경하다.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
-import { PureComponent } from 'react';
 import styles from './Header.module.css';
 
-export default class Header extends PureComponent {
-  render() {
-    return (
-      <header className={styles.gnb}>
-        <h1 className={styles.title}>점심 뭐 먹지</h1>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={styles.gnb}>
+      <h1 className={styles.title}>점심 뭐 먹지</h1>
+    </header>
+  );
+};
+
+export default Header;
