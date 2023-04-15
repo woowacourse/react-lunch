7489708928d3658461c64eae01784539a9f76f52
yeopto <gunyeop530@gmail.com>
2023-04-15 15:49:22 +0900

    refactor: RestaurantItem props를 비구조화 할당하다

--- src/components/RestaurantItem/RestaurantItem.tsx
+++ src/components/RestaurantItem/RestaurantItem.tsx
@@ -5,15 +5,17 @@ import styles from './RestaurantItem.module.css';
 const RestaurantItem = (props: RestaurantItemProps) => {
   const imageFile = CATEGORY_TO_FILENAME[props.category];
 
+  const { category, name, distance, description } = props;
+
   return (
     <>
       <div className={styles.category}>
-        <img src={imageFile} alt={props.category} className={styles.icon} />
+        <img src={imageFile} alt={category} className={styles.icon} />
       </div>
       <div className={styles.information}>
-        <h3 className={styles.subtitle}>{props.name}</h3>
-        <p className={styles.distance}>캠퍼스부터 {props.distance}분 내</p>
-        <p className={styles.description}>{props.description}</p>
+        <h3 className={styles.subtitle}>{name}</h3>
+        <p className={styles.distance}>캠퍼스부터 {distance}분 내</p>
+        <p className={styles.description}>{description}</p>
       </div>
     </>
   );
