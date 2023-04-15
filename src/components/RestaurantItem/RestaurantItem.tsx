925b4669a0ac6c6d18b1fe01f4d3764a5f108914
yeopto <gunyeop530@gmail.com>
2023-04-15 15:28:29 +0900

    refactor: RestaurantItem 컴포넌트를 함수형 컴포넌트로 변경하다

--- src/components/RestaurantItem/RestaurantItem.tsx
+++ src/components/RestaurantItem/RestaurantItem.tsx
@@ -1,23 +1,22 @@
-import { Component } from 'react';
 import { CATEGORY_TO_FILENAME } from '../../image/image';
 import { RestaurantItemProps } from '../../types/types';
 import styles from './RestaurantItem.module.css';
 
-export default class RestaurantItem extends Component<RestaurantItemProps> {
-  render() {
-    const imageFile = CATEGORY_TO_FILENAME[this.props.category];
+const RestaurantItem = (props: RestaurantItemProps) => {
+  const imageFile = CATEGORY_TO_FILENAME[props.category];
 
-    return (
-      <>
-        <div className={styles.category}>
-          <img src={imageFile} alt={this.props.category} className={styles.icon} />
-        </div>
-        <div className={styles.information}>
-          <h3 className={styles.subtitle}>{this.props.name}</h3>
-          <p className={styles.distance}>캠퍼스부터 {this.props.distance}분 내</p>
-          <p className={styles.description}>{this.props.description}</p>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className={styles.category}>
+        <img src={imageFile} alt={props.category} className={styles.icon} />
+      </div>
+      <div className={styles.information}>
+        <h3 className={styles.subtitle}>{props.name}</h3>
+        <p className={styles.distance}>캠퍼스부터 {props.distance}분 내</p>
+        <p className={styles.description}>{props.description}</p>
+      </div>
+    </>
+  );
+};
+
+export default RestaurantItem;
