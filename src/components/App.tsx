7e5f0ad10c6878e59d2ece5e4b744bca47c44268
jeongwusi <jeongwusi@gmail.com>
2023-04-16 07:46:05 +0900

    ♻️ refactor: App 컴포넌트를 클래스형에서 함수형으로 변경하다.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,13 @@
-import { Component } from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Main />
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
