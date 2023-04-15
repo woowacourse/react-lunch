87e64c33fd5072228c5b84a1e51e955ba8fbb6dc
JIWON <afds4567@gmail.com>
2023-04-16 00:17:10 +0900

    refactor(RestaurantDetailDrawer): drawer 렌더 위치 변경
    createPortal을 적용해 root로부터 독립된 위치에 drawer 렌더

--- src/RestaurantDetailDrawer.tsx
+++ src/RestaurantDetailDrawer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Drawer from './common/Drawer.tsx';
 import { Restaurant } from './util/type.js';
 import { CATEGORY_IMAGES, NO_EXIST_RESTAURANT } from './util/constant.ts';
@@ -17,6 +18,8 @@ class RestaurantDetailDrawer extends React.Component<
   RestaurantDetailDrawerProps,
   RestaurantDetailDrawerState
 > {
+  drawerContainer: HTMLDivElement = document.createElement('div');
+
   constructor(props) {
     super(props);
     this.state = {
@@ -24,7 +27,7 @@ class RestaurantDetailDrawer extends React.Component<
     };
   }
 
-  componentDidUpdate(prevProps:RestaurantDetailDrawerProps): void {
+    componentDidUpdate(prevProps: RestaurantDetailDrawerProps): void {
     if (this.props.restaurantId !== prevProps.restaurantId) {
       this.setState({
         restaurant: this.fetchRestaurantById()
@@ -32,39 +35,53 @@ class RestaurantDetailDrawer extends React.Component<
     }
   }
 
+  componentDidMount() {
+    const modalHook = document.getElementById('modal-hook');
+    if (modalHook) {
+      modalHook.appendChild(this.drawerContainer);
+    }
+  }
+
   fetchRestaurantById() {
     const rawRestaurantList = localStorage.getItem('restaurantList');
     if (!rawRestaurantList) return NO_EXIST_RESTAURANT;
     const restaurantList = JSON.parse(rawRestaurantList);
-    return restaurantList.find((restaurant:Restaurant) => +restaurant.id === +this.props.restaurantId) ?? NO_EXIST_RESTAURANT;
+    return (
+      restaurantList.find(
+        (restaurant: Restaurant) => +restaurant.id === +this.props.restaurantId
+      ) ?? NO_EXIST_RESTAURANT
+    );
   }
 
   render() {
-    return (
+    return ReactDOM.createPortal(
       <Drawer isOpenDrawer={this.props.isOpenDrawer}>
         <div className='restaurant__category'>
-          <img src={CATEGORY_IMAGES[this.state.restaurant.category]} alt={this.state.restaurant.category} className="category-icon" />
+          <img
+            src={CATEGORY_IMAGES[this.state.restaurant.category]}
+            alt={this.state.restaurant.category}
+            className='category-icon'
+          />
         </div>
-        <h3 className="restaurant__name text-subtitle">
+        <h3 className='restaurant__name text-subtitle'>
           {this.state.restaurant.title}
         </h3>
-        <span className="restaurant__distance text-body">
+        <span className='restaurant__distance text-body'>
           캠퍼스로부터 {this.state.restaurant.distance}분 내
         </span>
-        <p className="text-body">
-          {this.state.restaurant.description}
-        </p>
-        <p className="restaurant__link text-body">
+        <p className='text-body'>{this.state.restaurant.description}</p>
+        <p className='restaurant__link text-body'>
           {this.state.restaurant.link ?? ''}
         </p>
         <button
-          type="button"
-          className="button button--secondary text-caption"
+          type='button'
+          className='button button--secondary text-caption'
           onClick={() => this.props.onToggleDrawer()}
         >
           취소하기
         </button>
-      </Drawer>
+      </Drawer>,
+      this.drawerContainer
     );
   }
 }
