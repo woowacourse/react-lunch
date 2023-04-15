dc50b483302d7f10df891698aecbdd1f177e0074
Leejin-Yang <ejin1996@gmail.com>
2023-04-15 20:53:54 +0900

    refactor: 음식점 필터 및 정렬 hook으로 분리

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,28 +1,17 @@
-import { useState } from "react";
-
 import RestaurantFilterContainer from "./RestaurantFilterContainer";
 import RestaurantList from "./RestaurantList";
-import { CATEGORY_OPTIONS, SORTING_OPTIONS } from "../constants/options";
+import useRestaurants from "../hooks/useRestaurants";
 
 import styles from "./Main.module.css";
 
-interface Options {
-  category: string;
-  sorting: string;
-}
-
 const Main = () => {
-  const [options, setOptions] = useState<Options>({ category: CATEGORY_OPTIONS.TOTAL, sorting: SORTING_OPTIONS.NAME });
-
-  const setOption = (key: string, option: string) => {
-    setOptions((prev) => ({ ...prev, [key]: option }));
-  };
+  const { restaurants, setOption } = useRestaurants();
 
   return (
     <main className={styles.container}>
       <RestaurantFilterContainer setOption={setOption} />
       <section>
-        <RestaurantList options={options} />
+        <RestaurantList restaurants={restaurants} />
       </section>
     </main>
   );
--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,45 +1,16 @@
 import RestaurantItem from "./RestaurantItem";
-import mockData from "../assets/mockData.json";
 import type { Restaurant } from "../types/restaurant";
 
 import styles from "./RestaurantList.module.css";
-import { CATEGORY_OPTIONS, SORTING_OPTIONS } from "../constants/options";
-import { isCategory } from "../assets/images/category";
 
 interface RestaurantListProps {
-  options: { category: string; sorting: string };
+  restaurants: Restaurant[];
 }
 
-const restaurants: Restaurant[] = JSON.parse(localStorage.getItem("restaurant") ?? JSON.stringify(mockData));
-
 const RestaurantList = (props: RestaurantListProps) => {
-  const filterList = () => {
-    const { category } = props.options;
-
-    if (category === CATEGORY_OPTIONS.TOTAL) {
-      return restaurants;
-    }
-
-    if (category === CATEGORY_OPTIONS.ETC) {
-      return restaurants.filter((data) => !isCategory(data.category));
-    }
-
-    return restaurants.filter((data) => data.category === category);
-  };
-
-  const sortList = (restaurants: Restaurant[]) => {
-    const { sorting } = props.options;
-
-    if (sorting === SORTING_OPTIONS.NAME) {
-      return [...restaurants].sort((first, second) => first.name.localeCompare(second.name));
-    }
-
-    return [...restaurants].sort((first, second) => first.distance - second.distance);
-  };
-
   return (
     <ul className={styles.restaurantList}>
-      {sortList(filterList()).map((restaurant) => (
+      {props.restaurants.map((restaurant) => (
         <RestaurantItem key={restaurant.id} restaurant={restaurant} />
       ))}
     </ul>
