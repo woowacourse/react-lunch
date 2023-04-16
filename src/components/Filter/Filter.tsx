ff6c5f09f0ae4c86b518845f0d052715022c8ea6
고니_정우시 <jeongwusi@gmail.com>
2023-04-16 21:01:27 +0900

    [점심 뭐 먹지 미션 Step 2] 고니(정우시) 미션 제출합니다. (#62)
    * ♻️ refactor: App 컴포넌트를 클래스형에서 함수형으로 변경하다.
    
    * ♻️ refactor: Header 컴포넌트를 함수형으로 변경하다.
    
    * ♻️ refactor: RestaurantsItem 컴포넌트를 함수형으로 변경하다.
    
    * ♻️ refactor: RestaurantsList 컴포넌트를 함수형으로 변경하다.
    
    * ♻️ refactor:RestaurantFilter 컴포넌트를 함수형으로 변경하다.
    
    * ♻️ refactor: Modal 컴포넌트 함수형으로 변경하다.
    
    * ✨ feat: useEscapeKey 훅 생성하다.
    
    * ♻️ refactor: Main 컴포넌트 함수형으로 변경하다.
    
    * ♻️ refactor: Filter 컴포넌트 함수형으로 변경하다.
    
    * 🗑️ remove: 불필요한 코드 삭제하다.

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,23 +1,20 @@
-import { Component } from 'react';
 import { FilterProps } from '../../types/types';
 import styles from './Filter.module.css';
 
-export default class Filter extends Component<FilterProps> {
-  render() {
-    const { name, options, onChange } = this.props;
-
-    const optionElements = options.map(option => {
-      return (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      );
-    });
-
+const Filter = ({ name, options, onChange }: FilterProps) => {
+  const optionElements = options.map(option => {
     return (
-      <select className={styles.filter} name={name} onChange={onChange}>
-        {optionElements}
-      </select>
+      <option key={option.value} value={option.value}>
+        {option.label}
+      </option>
     );
-  }
-}
+  });
+
+  return (
+    <select className={styles.filter} name={name} onChange={onChange}>
+      {optionElements}
+    </select>
+  );
+};
+
+export default Filter;
