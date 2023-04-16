caae98aee6629f9d7abf527d7046275d25ce1f58
Dohyeon Lee <dlehgus1110@naver.com>
2023-04-16 17:41:47 +0900

    refactor: 음식점 객체 거리 타입 네이밍 수정

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 export type RestaurantCategory = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
-export type RestaurantDistance = '5' | '10' | '15' | '20' | '30';
+export type RequiredMinuteFromCampus = '5' | '10' | '15' | '20' | '30';
 
 export type Restaurant = {
   id: number;
   category: RestaurantCategory;
   name: string;
-  distance: RestaurantDistance;
+  distance: RequiredMinuteFromCampus;
   description: string;
   link: string;
 };
