19bc0d018794041557831bd4d189ae8e070f9f3b
chsua <csj1919@naver.com>
2023-04-16 16:03:36 +0900

    refactor: icon 및 레스토랑 아이템 컴포넌트 함수형으로 수정

--- src/components/MainLayout/RestaurantItem/index.tsx
+++ src/components/MainLayout/RestaurantItem/index.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 import './style.css';
 
 import CategoryIcon from '../../common/CategoryIcon';
@@ -10,31 +8,27 @@ interface Props {
   restaurant: Restaurant;
 }
 
-class RestaurantItem extends Component<Props> {
-  render() {
-    const { id, category, name, takeTime, description } = this.props.restaurant;
+export default function RestaurantItem(props: Props) {
+  const { id, category, name, takeTime, description } = props.restaurant;
 
-    return (
-      <li className="restaurant" data-id={id}>
-        <div className="restaurant__category">
-          <CategoryIcon category={category} />
-        </div>
-        <div className="restaurant__info">
-          <div className="flex">
-            <div>
-              <h2 className="restaurant__name text-subtitle">{name}</h2>
-              <span className="restaurant__takeTime text-body">
-                캠퍼스부터 {takeTime}분 내
-              </span>
-            </div>
+  return (
+    <li className="restaurant" data-id={id}>
+      <div className="restaurant__category">
+        <CategoryIcon category={category} />
+      </div>
+      <div className="restaurant__info">
+        <div className="flex">
+          <div>
+            <h2 className="restaurant__name text-subtitle">{name}</h2>
+            <span className="restaurant__takeTime text-body">
+              캠퍼스부터 {takeTime}분 내
+            </span>
           </div>
-          {description ? (
-            <p className="restaurant__description text-body">{description}</p>
-          ) : null}
         </div>
-      </li>
-    );
-  }
+        {description ? (
+          <p className="restaurant__description text-body">{description}</p>
+        ) : null}
+      </div>
+    </li>
+  );
 }
-
-export default RestaurantItem;
--- src/components/common/CategoryIcon/index.tsx
+++ src/components/common/CategoryIcon/index.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 import ChineseIcon from './ChineseIcon';
 import KoreanIcon from './KoreanIcon';
 import EtcIcon from './EtcIcon';
@@ -11,23 +9,19 @@ interface Props {
   category: string;
 }
 
-class CategoryIcon extends Component<Props> {
-  render() {
-    switch (this.props.category) {
-      case '한식':
-        return <KoreanIcon />;
-      case '중식':
-        return <ChineseIcon />;
-      case '일식':
-        return <JapaneseIcon />;
-      case '양식':
-        return <WesternIcon />;
-      case '아시안':
-        return <AsianIcon />;
-      case '기타':
-        return <EtcIcon />;
-    }
+export default function CategoryIcon(props: Props) {
+  switch (props.category) {
+    case '한식':
+      return <KoreanIcon />;
+    case '중식':
+      return <ChineseIcon />;
+    case '일식':
+      return <JapaneseIcon />;
+    case '양식':
+      return <WesternIcon />;
+    case '아시안':
+      return <AsianIcon />;
+    default:
+      return <EtcIcon />;
   }
 }
-
-export default CategoryIcon;
