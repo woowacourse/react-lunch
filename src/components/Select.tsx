31df4e0164cebb43f12e571300ee61fa55c1f1c1
Q Kim <qoli0703@gmail.com>
2023-04-16 21:23:54 +0900

    refactor: Select 컴포넌트 함수형으로 변경

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,19 +1,23 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent, HTMLAttributes } from 'react';
 import styles from './Select.module.css';
 
-class Select extends Component<{ options: string[]; onChange: (event: ChangeEvent<HTMLSelectElement>) => void }> {
-  render() {
-    const { options, onChange } = this.props;
-    return (
-      <select className={styles.select} onChange={onChange}>
-        {options.map((optionName: string) => (
-          <option key={optionName} value={optionName}>
-            {optionName}
-          </option>
-        ))}
-      </select>
-    );
-  }
+interface SelectProps extends HTMLAttributes<HTMLSelectElement> {
+  options: string[];
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void 
+}
+
+function Select(props: SelectProps) {
+  const { options, onChange } = props;
+
+  return (
+    <select className={styles.select} onChange={onChange}>
+      {options.map((optionName: string) => (
+        <option key={optionName} value={optionName}>
+          {optionName}
+        </option>
+      ))}
+    </select>
+  );
 }
 
 export default Select;
