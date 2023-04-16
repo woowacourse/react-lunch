60b641af806d15c54537680703f42d22b70a2a84
turtle601 <turtle601@naver.com>
2023-04-16 20:48:33 +0900

    style: 수정하지 않은 indent 4 스타일 2로 수정

--- src/store/domain/index.ts
+++ src/store/domain/index.ts
@@ -4,17 +4,24 @@ import { Restaurant, Sort, Category } from '../type';
 const { restaurantList } = mockData as { restaurantList: Restaurant[] };
 
 export const filterRestaurantList = (category: Category, sort: Sort) => {
-	const filteredList =
-		category === '전체' ? restaurantList : restaurantList.filter((restaurant) => restaurant.category === category);
+  const filteredList =
+    category === '전체'
+      ? restaurantList
+      : restaurantList.filter((restaurant) => restaurant.category === category);
 
-	if (sort === '거리순') {
-		return filteredList.sort((x, y) => Number(x.distance) - Number(y.distance));
-	}
+  if (sort === '거리순') {
+    return filteredList.sort((x, y) => Number(x.distance) - Number(y.distance));
+  }
 
-	return filteredList.sort((x, y) => x.name.localeCompare(y.name));
+  return filteredList.sort((x, y) => x.name.localeCompare(y.name));
 };
 
-export const sortRestaurantList = (stateForRestaurantList: Restaurant[], sort: Sort) =>
-	sort === '거리순'
-		? stateForRestaurantList.sort((x, y) => Number(x.distance) - Number(y.distance))
-		: stateForRestaurantList.sort((x, y) => x.name.localeCompare(y.name));
+export const sortRestaurantList = (
+  stateForRestaurantList: Restaurant[],
+  sort: Sort
+) =>
+  sort === '거리순'
+    ? stateForRestaurantList.sort(
+        (x, y) => Number(x.distance) - Number(y.distance)
+      )
+    : stateForRestaurantList.sort((x, y) => x.name.localeCompare(y.name));
