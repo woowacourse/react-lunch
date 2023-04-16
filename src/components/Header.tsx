d7fad7cf90109a7760de9a7df08277bc290fed9d
Q Kim <qoli0703@gmail.com>
2023-04-16 20:01:01 +0900

    refactor: 헤더 컴포넌트 함수형으로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,37 @@
-import React, { ChangeEvent, Component, ReactNode } from 'react';
+import React, { ChangeEvent, Component, KeyboardEvent, ReactNode } from 'react';
 import styles from './Header.module.css';
 import Select from './Select';
 
-class Header extends Component<{ onChange: (event: ChangeEvent<HTMLSelectElement>, kind: 'filter' | 'sort') => void }> {
-  static onTitleClick() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+interface HeaderProps { 
+  onChange: (event: ChangeEvent<HTMLSelectElement>, kind: 'filter' | 'sort') => void 
+}
 
-  render(): ReactNode {
-    const { onChange } = this.props;
+const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
-    return (
-      <header className={styles.header}>
-        <h1 onClick={Header.onTitleClick} aria-hidden="true" className={styles.title}>
-          점심 뭐 먹지
-        </h1>
-        <nav className={styles.nav}>
-          <Select
-            onChange={(event: ChangeEvent<HTMLSelectElement>) => onChange(event, 'filter')}
-            options={['전체', '한식', '일식', '중식', '아시안', '양식', '기타']}
-          />
-          <Select
-            onChange={(event: ChangeEvent<HTMLSelectElement>) => onChange(event, 'sort')}
-            options={['이름순', '거리순']}
-          />
-        </nav>
-      </header>
-    );
-  }
+const scrollTopKeyboardEventHandler = (event: KeyboardEvent<HTMLHeadingElement>) => {
+  if (event.key === 'Enter') scrollToTop();
+}
+
+function Header(props: HeaderProps) {
+  const { onChange } = props;
+
+  return (
+    <header className={styles.header}>
+      <h1 onClick={scrollToTop} onKeyUp={scrollTopKeyboardEventHandler} className={styles.title}>
+        점심 뭐 먹지
+      </h1>
+      <nav className={styles.nav}>
+        <Select
+          onChange={(event: ChangeEvent<HTMLSelectElement>) => onChange(event, 'filter')}
+          options={['전체', '한식', '일식', '중식', '아시안', '양식', '기타']}
+        />
+        <Select
+          onChange={(event: ChangeEvent<HTMLSelectElement>) => onChange(event, 'sort')}
+          options={['이름순', '거리순']}
+        />
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
