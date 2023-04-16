be1411ba6c01b84ff51755a1ee29e2f0f9299911
gilpop8663 <wolfye@naver.com>
2023-04-17 03:14:17 +0900

    feat: Select 컴포넌트 function 컴포넌트로 구현

--- docs/REQUIREMENTS_2.md
+++ docs/REQUIREMENTS_2.md
@@ -10,7 +10,7 @@
 - [x] RestaurantDetail
 - [x] RestaurantList
 - [x] RestaurantSummary
-- [ ] Select
+- [x] Select
 
 ## Custom Hooks을 이용하여 재사용 가능한 기능을 분리한다.
 
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,19 +1,19 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent } from 'react';
 import styles from './Select.module.css';
 
-class Select extends Component<{ options: string[]; onChange: (event: ChangeEvent<HTMLSelectElement>) => void }> {
-  render() {
-    const { options, onChange } = this.props;
-    return (
-      <select className={styles.select} onChange={onChange}>
-        {options.map((optionName: string) => (
-          <option key={optionName} value={optionName}>
-            {optionName}
-          </option>
-        ))}
-      </select>
-    );
-  }
+interface SelectProps {
+  options: string[];
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export default Select;
+export default function Select({ options, onChange }: SelectProps) {
+  return (
+    <select className={styles.select} onChange={onChange}>
+      {options.map((optionName: string) => (
+        <option key={optionName} value={optionName}>
+          {optionName}
+        </option>
+      ))}
+    </select>
+  );
+}
