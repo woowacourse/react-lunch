a2ef319cf37dfebd0528a08f06f0ceb3b1d3e98d
chsua <csj1919@naver.com>
2023-04-16 22:50:03 +0900

    refactor: select tag 파일에서 불필요한 코드 정리

--- src/components/common/Select/index.tsx
+++ src/components/common/Select/index.tsx
@@ -1,21 +1,12 @@
-import { ChangeEvent, Component } from 'react';
-
 interface Props {
   name: string;
   options: string[];
   onChange: (value: string) => void;
 }
 
-const handleChange = (
-  callback: (value: string) => void,
-  event: ChangeEvent<HTMLSelectElement>,
-) => {
-  callback(event.target.value);
-};
-
 export default function Select(props: Props) {
   return (
-    <select name={props.name} onChange={(e) => handleChange(props.onChange, e)}>
+    <select name={props.name} onChange={(e) => props.onChange(e.target.value)}>
       {props.options.map((option) => (
         <option key={option} value={option}>
           {option}
