2fe10554f71e453b0631d0c05911e5aa4a5cad1c
n0eyes <doll4586633@naver.com>
2023-04-16 18:07:34 +0900

    refactor: Select 컴포넌트 함수형으로 변경

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,25 +1,19 @@
-import React, { Component, ReactNode } from 'react';
 import St from './styled';
-
 interface SelectProps {
   options: readonly string[];
   onChange(value: string): void;
 }
 
-class Select extends Component<SelectProps> {
-  render(): ReactNode {
-    const { options, onChange, ...props } = this.props;
+export default function Select(props: SelectProps) {
+  const { options, onChange: onChangeSelect, ...restProps } = props;
 
-    return (
-      <St.Layout onChange={(e) => onChange(e.target.value)} {...props}>
-        {options.map((option) => (
-          <St.Option value={option} key={option}>
-            {option}
-          </St.Option>
-        ))}
-      </St.Layout>
-    );
-  }
+  return (
+    <St.Layout onChange={(e) => onChangeSelect(e.target.value)} {...restProps}>
+      {options.map((option) => (
+        <St.Option value={option} key={option}>
+          {option}
+        </St.Option>
+      ))}
+    </St.Layout>
+  );
 }
-
-export default Select;
