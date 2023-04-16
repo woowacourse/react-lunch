c10891c5dd6fa90f4f1d7c6b09f41abec8b2b2ef
guridaek <xylitol79@gmail.com>
2023-04-16 21:33:06 +0900

    refactor: RestaurantList 컴포넌트에 useCallback 사용

--- src/components/RestaurantList/index.tsx
+++ src/components/RestaurantList/index.tsx
@@ -3,7 +3,7 @@ import RestaurantItem from "../RestaurantItem";
 import { CategoryOption, Restaurant, SortOption } from "../../types/restaurant";
 import mockData from "../../data/mockData.json";
 import { LocalStorage } from "../../utils/LocalStorage";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const LOCAL_STORAGE_KEY = "RESTAURANT_LIST";
 
@@ -30,37 +30,37 @@ const RestaurantList = (props: RestaurantListProps) => {
   const [filteredRestaurants, setfilteredRestaurants] = useState(allRestaurants);
   const { selectedCategory, selectedSort } = props;
 
+  const getListByCategory = useCallback(
+    (restaurants: Restaurant[]) => {
+      if (selectedCategory === "all") {
+        return restaurants;
+      }
+
+      return restaurants.filter((restaurant) => restaurant.category === selectedCategory);
+    },
+    [selectedCategory]
+  );
+
+  const getSortedList = useCallback(
+    (restaurants: Restaurant[]) => {
+      if (selectedSort === "name") {
+        return getSortedListByName(restaurants);
+      }
+      if (selectedSort === "distance") {
+        return getSortedListByDistance(restaurants);
+      }
+
+      return restaurants;
+    },
+    [selectedSort]
+  );
+
   useEffect(() => {
     const restaurantListByCategory = getListByCategory(allRestaurants);
     const sortedRestaurantList = getSortedList(restaurantListByCategory);
 
     setfilteredRestaurants(sortedRestaurantList);
-  }, [selectedCategory]);
-
-  useEffect(() => {
-    const sortedRestaurantList = getSortedList(filteredRestaurants);
-
-    setfilteredRestaurants(sortedRestaurantList);
-  }, [selectedSort]);
-
-  const getListByCategory = (restaurants: Restaurant[]) => {
-    if (selectedCategory === "all") {
-      return restaurants;
-    }
-
-    return restaurants.filter((restaurant) => restaurant.category === selectedCategory);
-  };
-
-  const getSortedList = (restaurants: Restaurant[]) => {
-    if (selectedSort === "name") {
-      return getSortedListByName(restaurants);
-    }
-    if (selectedSort === "distance") {
-      return getSortedListByDistance(restaurants);
-    }
-
-    return restaurants;
-  };
+  }, [getListByCategory, getSortedList]);
 
   const getSortedListByName = (restaurants: Restaurant[]) => {
     return [...restaurants].sort((a, b) => a.name.localeCompare(b.name));
