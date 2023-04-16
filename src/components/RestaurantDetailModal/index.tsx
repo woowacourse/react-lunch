f79b52704bf5776f53e022c0fc33e30e36fb66d4
chsua <csj1919@naver.com>
2023-04-16 19:10:34 +0900

    refactor: restaurantDetailModal 컴포넌트를 함수형 컴포넌트로 수정

--- src/components/RestaurantDetailModal/index.tsx
+++ src/components/RestaurantDetailModal/index.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 import './style.css';
 
 import Modal from '../common/Modal';
@@ -8,60 +6,51 @@ import CategoryIcon from '../common/CategoryIcon';
 import { Restaurant } from '../../domain/type';
 
 interface Props {
-  restaurant: Restaurant | undefined;
+  restaurant: Restaurant;
   onCloseModal: () => void;
 }
 
-class RestaurantDetailModal extends Component<Props> {
-  render() {
-    if (!this.props.restaurant) return;
+export default function RestaurantDetailModal(props: Props) {
+  const { category, name, takeTime, description, link } = props.restaurant;
 
-    const { category, name, takeTime, description, link } =
-      this.props.restaurant;
-
-    return (
-      <Modal onCloseModal={this.props.onCloseModal}>
-        <div className="restaurant-detail-modal">
-          <div className="icon-container">
-            <div className="restaurant__category">
-              <CategoryIcon category={category} />
-            </div>
-          </div>
-          <h2 className="modal-title text-title detail-title">{name}</h2>
-          <span className="restaurant__takeTime text-body detail-takeTime">
-            캠퍼스부터 {takeTime}분 내
-          </span>
-          {description ? (
-            <div className="description">{description}</div>
-          ) : null}
-          {link ? (
-            <a
-              href={link}
-              target="_blank"
-              className="text-body link"
-              rel="noreferrer"
-            >
-              {link}
-            </a>
-          ) : null}
-          <div className="button-container detail-button-container">
-            <button
-              type="button"
-              className="button button--secondary text-caption"
-            >
-              삭제하기
-            </button>
-            <button
-              className="button button--primary text-caption"
-              onClick={this.props.onCloseModal}
-            >
-              닫기
-            </button>
+  return (
+    <Modal onCloseModal={props.onCloseModal}>
+      <div className="restaurant-detail-modal">
+        <div className="icon-container">
+          <div className="restaurant__category">
+            <CategoryIcon category={category} />
           </div>
         </div>
-      </Modal>
-    );
-  }
+        <h2 className="modal-title text-title detail-title">{name}</h2>
+        <span className="restaurant__takeTime text-body detail-takeTime">
+          캠퍼스부터 {takeTime}분 내
+        </span>
+        {description ? <div className="description">{description}</div> : null}
+        {link ? (
+          <a
+            href={link}
+            target="_blank"
+            className="text-body link"
+            rel="noreferrer"
+          >
+            {link}
+          </a>
+        ) : null}
+        <div className="button-container detail-button-container">
+          <button
+            type="button"
+            className="button button--secondary text-caption"
+          >
+            삭제하기
+          </button>
+          <button
+            className="button button--primary text-caption"
+            onClick={props.onCloseModal}
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
 }
-
-export default RestaurantDetailModal;
