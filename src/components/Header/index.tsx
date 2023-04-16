6a0899b33e1cb23cbd3802b0a4f9727fd8e8afb3
n0eyes <doll4586633@naver.com>
2023-04-16 17:40:37 +0900

    refactor: Header 컴포넌트 함수형으로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,5 @@
-import React, { Component, ReactNode } from 'react';
 import St from './styled';
 
-class Header extends Component {
-  render(): ReactNode {
-    return <St.Header>점심 뭐 먹지</St.Header>;
-  }
+export default function Header() {
+  return <St.Header>점심 뭐 먹지</St.Header>;
 }
-export default Header;
