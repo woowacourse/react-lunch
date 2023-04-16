a112f8a97718d6d6c47d560e33211a5388fc7bbd
Dohyeon Lee <dlehgus1110@naver.com>
2023-04-16 16:02:34 +0900

    refactor: 단축프로퍼티 적용

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ export default class App extends Component {
   };
 
   onChangeSortFilter = (sortOption: RestaurantSortOption) => {
-    this.setState({ sortOption: sortOption });
+    this.setState({ sortOption });
   };
 
   onClickRestaurantItem = (restaurantId: number) => {
