f7fa57b6def4cd5ddd80ea573a45077dd5bf5b8b
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-17 00:16:58 +0900

    refactor: 안쓰는 타입들 삭제

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,4 +1,4 @@
-import { ORDER_KEY, CATEGORY_NAME } from '../constants';
+import { CATEGORY_NAME } from '../constants';
 
 export interface RestaurantItemType {
   category: CategoryType;
@@ -8,16 +8,7 @@ export interface RestaurantItemType {
   link: string;
 }
 
-export interface RestaurantListStateType {
-  restaurants: RestaurantItemType[];
-  filteredRestaurants: RestaurantItemType[];
-  category: CategoryType;
-  order: OrderType;
-}
-
 export type CategoryType = (typeof CATEGORY_NAME)[keyof typeof CATEGORY_NAME];
-
-export type OrderType = (typeof ORDER_KEY)[keyof typeof ORDER_KEY];
 
 export enum SelectKind {
   category = '카테고리',
