13f80e3a7769934a05b7881c3812e22b11b7135d
n0eyes <doll4586633@naver.com>
2023-04-16 18:09:07 +0900

    refactor: SelectBar 컴포넌트 함수형으로 변경

--- src/components/SelectBar/index.tsx
+++ src/components/SelectBar/index.tsx
@@ -1,4 +1,3 @@
-import React, { Component, ReactNode } from 'react';
 import { ALIGN_FILTER, CATEGORY_FILTER } from '../../constants/restaurants';
 import { AlignFilter, CategoryFilter } from '../../types/restaurants';
 import Select from '../Select';
@@ -8,24 +7,21 @@ interface SelectBarProps {
   onChangeCategoryFilter(category: CategoryFilter): void;
   onChangeAlignFilter(align: AlignFilter): void;
 }
-class SelectBar extends Component<SelectBarProps> {
-  render(): ReactNode {
-    const { onChangeCategoryFilter, onChangeAlignFilter } = this.props;
-    return (
-      <St.Layout>
-        <Select
-          options={CATEGORY_FILTER}
-          onChange={onChangeCategoryFilter}
-          data-cy="category-select"
-        />
-        <Select
-          options={ALIGN_FILTER}
-          onChange={onChangeAlignFilter}
-          data-cy="align-select"
-        />
-      </St.Layout>
-    );
-  }
-}
 
-export default SelectBar;
+export default function SelectBar(props: SelectBarProps) {
+  const { onChangeCategoryFilter, onChangeAlignFilter } = props;
+  return (
+    <St.Layout>
+      <Select
+        options={CATEGORY_FILTER}
+        onChange={onChangeCategoryFilter}
+        data-cy="category-select"
+      />
+      <Select
+        options={ALIGN_FILTER}
+        onChange={onChangeAlignFilter}
+        data-cy="align-select"
+      />
+    </St.Layout>
+  );
+}
