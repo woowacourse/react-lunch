cd9869b597f45e3fcdebb0d7f5578c64504d8988
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-17 07:00:40 +0900

    refactor: Header 컴포넌트 styled-component 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
 export const Header = () => {
   return (
-    <header className="gnb">
-      <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-    </header>
+    <HeaderGnb>
+      <HeaderTitle className="text-title">점심 뭐 먹지</HeaderTitle>
+    </HeaderGnb>
   );
 };
+
+const HeaderGnb = styled.header`
+  display: flex;
+  align-items: center;
+  height: 64px;
+
+  padding: 0 16px;
+  background-color: var(--primary-color);
+`;
+
+const HeaderTitle = styled.h1`
+  color: #fcfcfd;
+`;
