d797f74954aa7a980e8e348ec341371dc4192be5
nangkyeonglim <nangkyeonglim@gmail.com>
2023-04-16 14:27:16 +0900

    fix: sort 원본 배열이 정렬되지 않도록 수정

--- src/domain/restaurant.ts
+++ src/domain/restaurant.ts
@@ -26,7 +26,7 @@ const restaurant = {
 
   sort: (restaurants: Restaurant[], type: string): Restaurant[] => {
     if (type === SORT_OPTIONS.NAME) return restaurants.sort((a, b) => (a.name > b.name ? 1 : -1));
-    return restaurants.sort((a, b) => (a.distanceByMinutes > b.distanceByMinutes ? 1 : -1));
+    return [...restaurants].sort((a, b) => (a.distanceByMinutes > b.distanceByMinutes ? 1 : -1));
   },
 };
 
