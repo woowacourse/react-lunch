31069434b11adeb6bc53b05071ceec0a2cd4d678
Q Kim <qoli0703@gmail.com>
2023-04-16 19:49:31 +0900

    refactor(components): 버튼 컴포넌트 함수형으로 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { HTMLAttributes } from 'react';
 import styles from './Button.module.css';
 
-class Button extends Component<{ text: string; kind: 'primary' | 'secondary'; onClick: () => void }> {
-  render() {
-    const { text, kind, onClick } = this.props;
-    return (
-      <button type="button" className={styles[kind]} onClick={onClick}>
-        {text}
-      </button>
-    );
-  }
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  text: string;
+  kind: 'primary' | 'secondary';
+}
+
+function Button(props: ButtonProps) {
+  const { text, kind, onClick } = props;
+
+  return (
+    <button type="button" className={styles[kind]} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
+
