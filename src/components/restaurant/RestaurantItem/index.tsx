d94ee4433c03c5db93d7f4272d8414d70c78df22
nlom0218 <nlom0218@naver.com>
2023-04-16 14:33:21 +0900

    refactor: RestaurantItem 컴포넌트 함수형으로 수정

--- src/components/restaurant/RestaurantItem/index.tsx
+++ src/components/restaurant/RestaurantItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import * as S from './style';
 import { Restaurant } from '../../../@types/type';
@@ -9,25 +9,21 @@ type Props = {
   openModal: (id: number) => void;
 };
 
-class RestaurantItem extends Component<Props> {
-  onClickRestaurant = () => {
-    this.props.openModal(this.props.restaurant.id);
+const RestaurantItem = ({ restaurant: { category, name, distanceByMinutes, description, id }, openModal }: Props) => {
+  const onClickRestaurant = () => {
+    openModal(id);
   };
 
-  render() {
-    const { category, name, distanceByMinutes, description } = this.props.restaurant;
-
-    return (
-      <S.RestaurantItem onClick={this.onClickRestaurant}>
-        <CategoryIcon category={category} />
-        <S.InformationContainer>
-          <S.RestaurantTitle>{name}</S.RestaurantTitle>
-          <S.RestaurantDistance>캠퍼스부터 {distanceByMinutes}분 내</S.RestaurantDistance>
-          <S.RestaurantDescription>{description}</S.RestaurantDescription>
-        </S.InformationContainer>
-      </S.RestaurantItem>
-    );
-  }
-}
+  return (
+    <S.RestaurantItem onClick={onClickRestaurant}>
+      <CategoryIcon category={category} />
+      <S.InformationContainer>
+        <S.RestaurantTitle>{name}</S.RestaurantTitle>
+        <S.RestaurantDistance>캠퍼스부터 {distanceByMinutes}분 내</S.RestaurantDistance>
+        <S.RestaurantDescription>{description}</S.RestaurantDescription>
+      </S.InformationContainer>
+    </S.RestaurantItem>
+  );
+};
 
 export default RestaurantItem;
