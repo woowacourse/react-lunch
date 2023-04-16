0e965ce1aa91b9266698e6fa77c947364674e74e
semnil5202 <semnil5202@gmail.com>
2023-04-16 20:48:11 +0900

    refactor(Header): 불필요한 React Fragment 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,9 @@ import { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <>
-        <header className="gnb">
-          <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-        </header>
-      </>
+      <header className="gnb">
+        <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+      </header>
     );
   }
 }
