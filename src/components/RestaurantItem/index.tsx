b628d976ee4a4bacb659febc5ed93aee0eef03da
hafnium1923 <hafnium1923@gmail.com>
2023-04-16 21:43:22 +0900

    refactor: RestaurantItem Component class to function migration

--- src/components/RestaurantItem/index.tsx
+++ src/components/RestaurantItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from "react";
+import React from "react";
 import St from "./styled";
 import CategoryImg from "../CategoryImg";
 import { Restaurant } from "../../types/restaurants";
@@ -7,29 +7,20 @@ interface RestaurantItemProps {
   info: Restaurant;
   onClick(): void;
 }
-class RestaurantItem extends Component<RestaurantItemProps> {
-  constructor(props: RestaurantItemProps) {
-    super(props);
-  }
-  render(): ReactNode {
-    const {
-      info: { title, detail, distance, category },
-      onClick,
-    } = this.props;
-
-    return (
-      <St.Layout onClick={onClick}>
-        <St.LeftSection>
-          <CategoryImg category={category} />
-        </St.LeftSection>
-        <St.RightSection>
-          <St.Title>{title}</St.Title>
-          <St.Distance>캠퍼스 부터 {distance}분</St.Distance>
-          <St.Detail>{detail}</St.Detail>
-        </St.RightSection>
-      </St.Layout>
-    );
-  }
+export default function RestaurantItem({
+  info: { title, detail, distance, category },
+  onClick,
+}: RestaurantItemProps) {
+  return (
+    <St.Layout onClick={onClick}>
+      <St.LeftSection>
+        <CategoryImg category={category} />
+      </St.LeftSection>
+      <St.RightSection>
+        <St.Title>{title}</St.Title>
+        <St.Distance>캠퍼스 부터 {distance}분</St.Distance>
+        <St.Detail>{detail}</St.Detail>
+      </St.RightSection>
+    </St.Layout>
+  );
 }
-
-export default RestaurantItem;
