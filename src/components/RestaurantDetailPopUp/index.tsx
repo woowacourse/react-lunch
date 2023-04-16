04f131c9798dcff3915900a2b62f01ce0625be6d
hafnium1923 <hafnium1923@gmail.com>
2023-04-16 21:40:34 +0900

    refactor: RestaurantDetailPopUp Component class to function migration

--- src/components/RestaurantDetailPopUp/index.tsx
+++ src/components/RestaurantDetailPopUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from "react";
+import React, { useEffect } from "react";
 import { Restaurant } from "../../types/restaurants";
 import CategoryImg from "../CategoryImg";
 import St from "./styled";
@@ -8,40 +8,33 @@ interface RestaurantDetailPopUpProps {
   close: VoidFunction;
 }
 
-class RestaurantDetailPopUp extends Component<RestaurantDetailPopUpProps> {
-  escHandler(e: KeyboardEvent) {
-    if (e.key === "Escape") this.props.close();
-  }
+export default function RestaurantDetailPopUp({
+  restaurant: { title, distance, detail, link, category },
+  close,
+}: RestaurantDetailPopUpProps) {
+  const escHandler = (e: KeyboardEvent) => {
+    if (e.key === "Escape") close();
+  };
 
-  componentDidMount(): void {
-    window.addEventListener("keyup", this.escHandler.bind(this));
-  }
+  useEffect(() => {
+    window.addEventListener("keyup", escHandler);
 
-  componentWillUnmount(): void {
-    window.removeEventListener("keyup", this.escHandler.bind(this));
-  }
+    return () => window.removeEventListener("keyup", escHandler);
+  });
 
-  render(): ReactNode {
-    const {
-      restaurant: { title, distance, detail, link, category },
-      close,
-    } = this.props;
-    return (
-      <>
-        <St.Backdrop onClick={close} />
-        <St.PopUp>
-          <CategoryImg category={category} />
-          <St.Detail>
-            <St.Title>{title}</St.Title>
-            <St.Distance>캠퍼스부터 {distance}분 이내</St.Distance>
-            <St.Description>{detail}</St.Description>
-            <St.Link href={link}>{link}</St.Link>
-            <St.Button onClick={close}>닫기</St.Button>
-          </St.Detail>
-        </St.PopUp>
-      </>
-    );
-  }
+  return (
+    <>
+      <St.Backdrop onClick={close} />
+      <St.PopUp>
+        <CategoryImg category={category} />
+        <St.Detail>
+          <St.Title>{title}</St.Title>
+          <St.Distance>캠퍼스부터 {distance}분 이내</St.Distance>
+          <St.Description>{detail}</St.Description>
+          <St.Link href={link}>{link}</St.Link>
+          <St.Button onClick={close}>닫기</St.Button>
+        </St.Detail>
+      </St.PopUp>
+    </>
+  );
 }
-
-export default RestaurantDetailPopUp;
