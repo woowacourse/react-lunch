229d7d934a6679749cb9710bbf6c526c9ada4cfe
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-16 23:46:04 +0900

    refactor: RestaurantList 함수형 컴포넌트로 변경

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,127 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import mockData from '../mockData.json';
 import RestaurantItem from './RestaurantItem';
-import { RestaurantItemType, RestaurantListStateType, SelectKind } from '../types';
+import { RestaurantItemType, SelectKind } from '../types';
 import { CATEGORY_NAME, ORDER_KEY, LOCAL_STORAGE_RESTAURANTS_KEY } from '../constants';
 import SelectBox from './common/SelectBox';
-import { $ } from '../utils/domSelector';
+import { getLocalStorage, setLocalStorage } from '../utils';
 
-const data: RestaurantItemType[] = JSON.parse(JSON.stringify(mockData));
+const RestaurantList = () => {
+  const [filteredRestaurants, setFilteredRestaurants] = useState<RestaurantItemType[]>([]);
+  const [category, setCategory] = useState<string>(CATEGORY_NAME.all);
+  const [order, setOrder] = useState<string>(ORDER_KEY.name);
 
-class RestaurantList extends React.Component<object, RestaurantListStateType> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      restaurants: [],
-      filteredRestaurants: [],
-      category: CATEGORY_NAME.all,
-      order: ORDER_KEY.name,
-    };
-  }
+  useEffect(() => {
+    setLocalStorage(LOCAL_STORAGE_RESTAURANTS_KEY, JSON.parse(JSON.stringify(mockData)));
+    setFilteredRestaurants(getLocalStorage(LOCAL_STORAGE_RESTAURANTS_KEY));
+  }, []);
 
-  handleClickEvent() {
-    $<HTMLElement>('#header_title').addEventListener('click', () => {
-      this.initSelectOption();
-    });
-  }
+  useEffect(() => {
+    setFilteredRestaurants(getSortedRestaurants(getFilteredRestaurants()));
+    window.scrollTo(0, 0);
+  }, [category, order]);
 
-  initSelectOption() {
-    const categorySelectBox = $<HTMLSelectElement>(`select[name="${SelectKind.category}"]`);
-    categorySelectBox.selectedIndex = 0;
-    categorySelectBox.dispatchEvent(new Event('change', { bubbles: true }));
-
-    const orderSelectBox = $<HTMLSelectElement>(`select[name="${SelectKind.order}"]`);
-    orderSelectBox.selectedIndex = 0;
-    orderSelectBox.dispatchEvent(new Event('change', { bubbles: true }));
-
-    this.handleClickEvent();
-  }
-
-  componentDidMount() {
-    this.setLocalStorage(data);
-    this.setState({ restaurants: this.getLocalStorage(), filteredRestaurants: this.getLocalStorage() });
-    this.handleClickEvent();
-  }
-
-  getLocalStorage(): RestaurantItemType[] {
-    const restaurants = localStorage.getItem(LOCAL_STORAGE_RESTAURANTS_KEY);
-    return restaurants ? JSON.parse(restaurants) : [];
-  }
-
-  setLocalStorage(data: RestaurantItemType[]) {
-    localStorage.setItem(LOCAL_STORAGE_RESTAURANTS_KEY, JSON.stringify(data));
-  }
-
-  sortRestaurants() {
-    const restaurants = this.state.filteredRestaurants;
-    restaurants.sort((a: RestaurantItemType, b: RestaurantItemType): number => {
-      if (this.state.order === ORDER_KEY.name) return a.name > b.name ? 1 : -1;
+  const getSortedRestaurants = (restaurants: RestaurantItemType[]) => {
+    const sortedRestaurants = restaurants.sort((a: RestaurantItemType, b: RestaurantItemType): number => {
+      if (order === ORDER_KEY.name) return a.name > b.name ? 1 : -1;
       return a.distance > b.distance ? 1 : -1;
     });
-    this.setState({ filteredRestaurants: restaurants });
-  }
 
-  filterRestaurants() {
-    if (this.state.category === CATEGORY_NAME.all) {
-      this.setState({ filteredRestaurants: this.state.restaurants });
-      return;
-    }
+    return sortedRestaurants;
+  };
 
-    const filteredRestaurants = this.state.restaurants.filter(
-      (restaurant) => restaurant.category === this.state.category
-    );
-    this.setState({ filteredRestaurants: filteredRestaurants });
-  }
+  const getFilteredRestaurants = () => {
+    const restaurants = getLocalStorage(LOCAL_STORAGE_RESTAURANTS_KEY);
 
-  componentDidUpdate(prevProps: object, prevStates: RestaurantListStateType) {
-    if (prevStates.category !== this.state.category) {
-      this.filterRestaurants();
-      window.scrollTo(0, 0);
-    }
-    if (prevStates.order !== this.state.order || prevStates.filteredRestaurants !== this.state.filteredRestaurants) {
-      this.sortRestaurants();
-      window.scrollTo(0, 0);
-    }
-  }
+    return category === CATEGORY_NAME.all
+      ? restaurants
+      : restaurants.filter((restaurant: RestaurantItemType) => restaurant.category === category);
+  };
 
-  render() {
-    return (
-      <>
-        <SelectBoxWrapper>
-          <SelectBox
-            selectType={SelectKind.category}
-            options={Object.values(CATEGORY_NAME)}
-            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-              this.setState({ category: event.target.value });
-            }}
-          />
-          <SelectBox
-            selectType={SelectKind.order}
-            options={Object.values(ORDER_KEY)}
-            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-              this.setState({ order: event.target.value });
-            }}
-          />
-        </SelectBoxWrapper>
-        <RestaurantListWrapper>
-          {this.state.filteredRestaurants.map((restaurant: RestaurantItemType, index: number) => {
-            return (
-              <RestaurantItem
-                key={index}
-                category={restaurant.category}
-                name={restaurant.name}
-                distance={restaurant.distance}
-                description={restaurant.description}
-                link={restaurant.link}
-              />
-            );
-          })}
-        </RestaurantListWrapper>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <SelectBoxWrapper>
+        <SelectBox
+          selectType={SelectKind.category}
+          options={Object.values(CATEGORY_NAME)}
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setCategory(event.target.value)}
+        />
+        <SelectBox
+          selectType={SelectKind.order}
+          options={Object.values(ORDER_KEY)}
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setOrder(event.target.value)}
+        />
+      </SelectBoxWrapper>
+      <RestaurantListWrapper>
+        {filteredRestaurants.map((restaurant: RestaurantItemType, index: number) => {
+          return (
+            <RestaurantItem
+              key={index}
+              category={restaurant.category}
+              name={restaurant.name}
+              distance={restaurant.distance}
+              description={restaurant.description}
+              link={restaurant.link}
+            />
+          );
+        })}
+      </RestaurantListWrapper>
+    </>
+  );
+};
 
 const RestaurantListWrapper = styled.div`
   display: flex;
