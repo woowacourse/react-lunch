d2429e59de68362c847a87c264f0483e8ade0acd
hafnium1923 <hafnium1923@gmail.com>
2023-04-16 21:27:41 +0900

    refactor: common/Layout Component class to function migration

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,20 +1,12 @@
-import React, { Component, PropsWithChildren, ReactNode } from "react";
+import React, { PropsWithChildren } from "react";
 import Header from "../../Header";
 import St from "./styled";
 
-class Layout extends Component<PropsWithChildren> {
-  constructor(props: PropsWithChildren) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <St.Layout>
-        <Header />
-        <St.Main>{this.props.children}</St.Main>
-      </St.Layout>
-    );
-  }
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <St.Layout>
+      <Header />
+      <St.Main>{children}</St.Main>
+    </St.Layout>
+  );
 }
-
-export default Layout;
