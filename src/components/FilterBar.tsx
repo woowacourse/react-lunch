3b18443aed6bb6bc64c134255f8233bef275c710
semnil5202 <semnil5202@gmail.com>
2023-04-16 23:46:41 +0900

    refactor(FilterBar): 함수형으로 변환

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,4 +1,3 @@
-import { PureComponent } from 'react';
 import {
   RESTAURANT_CATEGORY,
   SORTING_OPTION,
@@ -9,36 +8,33 @@ interface FilterBarProps {
   onChangeSort: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export default class FilterBar extends PureComponent<FilterBarProps> {
-  constructor(props: FilterBarProps) {
-    super(props);
-  }
+const FilterBar = (props: FilterBarProps) => {
+  const { onChangeCategory, onChangeSort } = props;
 
-  render() {
-    const { onChangeCategory, onChangeSort } = this.props;
-    return (
-      <section className="restaurant-filter-container">
-        <select
-          onChange={onChangeCategory}
-          name="category"
-          id="category-filter"
-          className="restaurant-filter"
-        >
-          {Object.values(RESTAURANT_CATEGORY).map((category) => (
-            <option value={category}>{category}</option>
-          ))}
-        </select>
-        <select
-          onChange={onChangeSort}
-          name="sorting"
-          id="sorting-filter"
-          className="restaurant-filter"
-        >
-          {Object.values(SORTING_OPTION).map((sort) => (
-            <option value={sort}>{sort}</option>
-          ))}
-        </select>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="restaurant-filter-container">
+      <select
+        onChange={onChangeCategory}
+        name="category"
+        id="category-filter"
+        className="restaurant-filter"
+      >
+        {Object.values(RESTAURANT_CATEGORY).map((category) => (
+          <option value={category}>{category}</option>
+        ))}
+      </select>
+      <select
+        onChange={onChangeSort}
+        name="sorting"
+        id="sorting-filter"
+        className="restaurant-filter"
+      >
+        {Object.values(SORTING_OPTION).map((sort) => (
+          <option value={sort}>{sort}</option>
+        ))}
+      </select>
+    </section>
+  );
+};
+
+export default FilterBar;
