4b6231a4ad99ddf47e1732f27a4e2769763dab18
ashleysyheo <ashleysyheo@gmail.com>
2023-04-17 15:30:00 +0900

    chore: 공백 정리

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,6 +1,7 @@
 function getLocalStorage(key: string) {
   return JSON.parse(localStorage.getItem(key) as string);
 }
+
 function saveToLocalStorage<T>(key: string, data: T[]) {
   localStorage.setItem(key, JSON.stringify(data));
 }
