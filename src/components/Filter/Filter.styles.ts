0cd619413eecdfa69fa63d44f1833f6ce9d8a0fb
Jungwoo <evencoding@gmail.com>
2023-04-17 14:25:14 +0900

    refactor: main 태그의 1개의 자식 컴포넌트를 Filter, RestaurantList로 분리

---
+++ src/components/Filter/Filter.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const FilterContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  margin-top: 24px;
+  padding: 0 16px;
+
+  select {
+    height: 44px;
+    min-width: 125px;
+
+    border: 1px solid #d0d5dd;
+    border-radius: 8px;
+    padding: 8px;
+
+    background: transparent;
+    font-size: 16px;
+  }
+`;
--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,61 +1,28 @@
-import { Category, Restaurant, SortingType } from '../../types/restaurant';
+import { Restaurant } from '../../types/restaurant';
 
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import * as styled from './RestaurantList.styles';
 import { RestaurantItem } from '..';
-
-import { CATEGORIES, SORTING_TYPES } from '../../constants';
 
 interface Props {
   restaurants: Restaurant[];
   openModal: (id: Restaurant['id']) => void;
-  setCategory: (category: Category) => void;
-  setSortingType: (sortingType: SortingType) => void;
 }
 
 const RestaurantList = (props: Props) => {
-  const openDetailModal = ({ target }: React.MouseEvent) => {
+  const onClickRestaurantList = ({ target }: React.MouseEvent) => {
     const { id } = (target as HTMLUListElement).closest('li');
     props.openModal(id);
   };
 
-  const onChangeCategory = ({ target: { value: category } }: ChangeEvent<HTMLSelectElement>) => {
-    props.setCategory(category as Category);
-  };
-
-  const onChangeSortingType = ({
-    target: { value: sortingType },
-  }: ChangeEvent<HTMLSelectElement>) => {
-    props.setSortingType(sortingType as SortingType);
-  };
-
   return (
-    <main>
-      <styled.FilterContainer>
-        <select name="category" id="category-filter" onChange={onChangeCategory}>
-          {['전체', ...CATEGORIES].map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-        <select name="sorting" id="sorting-filter" onChange={onChangeSortingType}>
-          {SORTING_TYPES.map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </styled.FilterContainer>
-
-      <styled.RestaurantListContainer>
-        <ul onClick={openDetailModal}>
-          {props.restaurants.map((restaurant) => (
-            <RestaurantItem key={restaurant.id} restaurant={restaurant}></RestaurantItem>
-          ))}
-        </ul>
-      </styled.RestaurantListContainer>
-    </main>
+    <styled.RestaurantListContainer>
+      <ul onClick={onClickRestaurantList}>
+        {props.restaurants.map((restaurant) => (
+          <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </ul>
+    </styled.RestaurantListContainer>
   );
 };
 
