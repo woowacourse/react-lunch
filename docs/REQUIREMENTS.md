48d4645133cd2f926a8c1d0b7b5061686ee06613
hafnium1923 <hafnium1923@gmail.com>
2023-04-17 15:45:59 +0900

    feat: localStorage에서 데이터 가져오기

--- docs/REQUIREMENTS.md
+++ docs/REQUIREMENTS.md
@@ -2,3 +2,4 @@
 
 - [x] Step1의 Class Component를 Function Component로 마이그레이션 한다
 - [x] Custom Hooks을 이용하여 재사용 가능한 기능을 분리한다.
+- [x] localStorage확인 후 없으면 mock 데이터 넣어두기
--- src/components/RestaurantList/useRestaurant.ts
+++ src/components/RestaurantList/useRestaurant.ts
@@ -7,6 +7,7 @@ import {
 import { fetchMockRestaurants } from "../../api/restaurants";
 import { alignBy, filterBy } from "../../domain/restaurants";
 import { BY_NAME } from "../../constants/restaurants";
+import { convertTypeAcquisitionFromJson } from "typescript";
 
 export default function useRestaurant(
   category: CategoryFilter,
@@ -20,11 +21,25 @@ export default function useRestaurant(
   useEffect(() => {
     const setData = async () => {
       const data = await fetchMockRestaurants({ align: BY_NAME });
-      setRestaurantList(data);
-      setRestaurantListOrigin(data);
+      localStorage.setItem("restaurantListOrigin", JSON.stringify(data));
+      setList(JSON.stringify(data));
     };
 
-    setData();
+    const setList = async (data: string) => {
+      const list = await JSON.parse(data);
+      setRestaurantList(list);
+      setRestaurantListOrigin(list);
+    };
+
+    if (!localStorage.getItem("restaurantListOrigin")) {
+      setData();
+    }
+
+    const data = localStorage.getItem("restaurantListOrigin");
+    console.log(data);
+    if (data) {
+      setList(data);
+    }
   }, []);
 
   useEffect(() => {
