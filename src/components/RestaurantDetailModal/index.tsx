bd50c97976f047d515b5e2243c3de35431e7282d
woo-jk <201702042@o.cnu.ac.kr>
2023-04-17 14:26:49 +0900

    refactor: RestaurantDetailModal 컴포넌트 함수형으로 마이그레이션

--- src/components/RestaurantDetailModal/index.tsx
+++ src/components/RestaurantDetailModal/index.tsx
@@ -4,39 +4,38 @@ import { CategoryImagePath } from "../../data/CategoryImagePath";
 import { Restaurant } from "../../types/restaurant";
 
 interface RestaurantDetailModalProps {
-  closeModal: () => void;
   restaurant: Restaurant;
+  closeModal: () => void;
 }
 
-export default class RestaurantDetailModal extends Component<RestaurantDetailModalProps> {
-  render() {
-    const { closeModal, restaurant } = this.props;
-    const { name, category, distance, description, link } = restaurant;
+const RestaurantDetailModal = ({ restaurant, closeModal }: RestaurantDetailModalProps) => {
+  const { name, category, distance, description, link } = restaurant;
 
-    return (
-      <div className="modal">
-        <div className="modal-backdrop" onClick={closeModal}></div>
-        <div className="modal-container">
-          <div className="restaurant-detail-container">
-            <div className="category-and-favorite">
-              <div className="restaurant__category">
-                <img src={CategoryImagePath[category]} alt={category} className="category-icon" />
-              </div>
+  return (
+    <div className="modal">
+      <div className="modal-backdrop" onClick={closeModal}></div>
+      <div className="modal-container">
+        <div className="restaurant-detail-container">
+          <div className="category-and-favorite">
+            <div className="restaurant__category">
+              <img src={CategoryImagePath[category]} alt={category} className="category-icon" />
             </div>
-            <h3 className="restaurant__name text-subtitle detail-subtitle">{name}</h3>
-            <span className="restaurant__distance text-body">캠퍼스부터 {distance}분 내</span>
-            <p className="restaurant__description text-body">{description}</p>
-            <a href={link} target="_blank">
-              {link}
-            </a>
-            <div className="button-container">
-              <button id="detail-cancel-button" className="button button--primary text-caption" onClick={closeModal}>
-                닫기
-              </button>
-            </div>
+          </div>
+          <h3 className="restaurant__name text-subtitle detail-subtitle">{name}</h3>
+          <span className="restaurant__distance text-body">캠퍼스부터 {distance}분 내</span>
+          <p className="restaurant__description text-body">{description}</p>
+          <a href={link} target="_blank">
+            {link}
+          </a>
+          <div className="button-container">
+            <button id="detail-cancel-button" className="button button--primary text-caption" onClick={closeModal}>
+              닫기
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default RestaurantDetailModal;
