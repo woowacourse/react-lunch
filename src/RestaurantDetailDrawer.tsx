6c3157437ed991e8ed130a96d9b5028d1c2f2dd7
JIWON <afds4567@gmail.com>
2023-04-17 15:13:30 +0900

    refactor(RestaurantDetailDrawer): 함수형 컴포넌트로 변경

--- src/RestaurantDetailDrawer.tsx
+++ src/RestaurantDetailDrawer.tsx
@@ -1,93 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import Drawer from './common/Drawer.tsx';
 import { Restaurant } from './util/type.js';
 import { CATEGORY_IMAGES, NO_EXIST_RESTAURANT } from './util/constant.ts';
 
-type RestaurantDetailDrawerProps = {
-  isOpenDrawer: boolean;
-  restaurantId: number;
-  onToggleDrawer: React.MouseEventHandler<HTMLButtonElement>;
-};
+const RestaurantDetailDrawer= ({
+  isOpenDrawer,
+  restaurantId,
+  onToggleDrawer,
+}) => {
+  const [restaurant, setRestaurant] = useState<Omit<Restaurant, 'id'>>(NO_EXIST_RESTAURANT);
 
-type RestaurantDetailDrawerState = {
-  restaurant: Omit<Restaurant, "id">;
-}
-
-class RestaurantDetailDrawer extends React.Component<
-  RestaurantDetailDrawerProps,
-  RestaurantDetailDrawerState
-> {
-  drawerContainer: HTMLDivElement = document.createElement('div');
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      restaurant: this.fetchRestaurantById()
-    };
-  }
-
-    componentDidUpdate(prevProps: RestaurantDetailDrawerProps): void {
-    if (this.props.restaurantId !== prevProps.restaurantId) {
-      this.setState({
-        restaurant: this.fetchRestaurantById()
-      });
-    }
-  }
-
-  componentDidMount() {
-    const modalHook = document.getElementById('modal-hook');
-    if (modalHook) {
-      modalHook.appendChild(this.drawerContainer);
-    }
-  }
-
-  fetchRestaurantById() {
+  const fetchRestaurantById = useCallback((): Omit<Restaurant, 'id'> => {
     const rawRestaurantList = localStorage.getItem('restaurantList');
     if (!rawRestaurantList) return NO_EXIST_RESTAURANT;
-    const restaurantList = JSON.parse(rawRestaurantList);
+    const restaurantList: Restaurant[] = JSON.parse(rawRestaurantList);
     return (
       restaurantList.find(
-        (restaurant: Restaurant) => +restaurant.id === +this.props.restaurantId
+        (restaurant: Restaurant) => +restaurant.id === +restaurantId
       ) ?? NO_EXIST_RESTAURANT
     );
-  }
+  }, [restaurantId]);
 
-  shouldComponentUpdate(nextProps: RestaurantDetailDrawerProps, nextState: RestaurantDetailDrawerState) {
-    return nextProps.isOpenDrawer !== this.props.isOpenDrawer || nextState.restaurant !== this.state.restaurant;
-  }
-  
-  render() {
-    return ReactDOM.createPortal(
-      <Drawer isOpenDrawer={this.props.isOpenDrawer}>
+  useEffect(() => {
+    setRestaurant(fetchRestaurantById());
+  }, [fetchRestaurantById]);
+
+  return isOpenDrawer ? (
+    ReactDOM.createPortal(
+      <Drawer isOpenDrawer={isOpenDrawer}>
         <div className='restaurant__category'>
           <img
-            src={CATEGORY_IMAGES[this.state.restaurant.category]}
-            alt={this.state.restaurant.category}
+            src={CATEGORY_IMAGES[restaurant.category]}
+            alt={restaurant.category}
             className='category-icon'
           />
         </div>
-        <h3 className='restaurant__name text-subtitle'>
-          {this.state.restaurant.title}
-        </h3>
+        <h3 className='restaurant__name text-subtitle'>{restaurant.title}</h3>
         <span className='restaurant__distance text-body'>
-          캠퍼스로부터 {this.state.restaurant.distance}분 내
+          캠퍼스로부터 {restaurant.distance}분 내
         </span>
-        <p className='text-body'>{this.state.restaurant.description}</p>
-        <p className='restaurant__link text-body'>
-          {this.state.restaurant.link ?? ''}
-        </p>
+        <p className='text-body'>{restaurant.description}</p>
+        <p className='restaurant__link text-body'>{restaurant.link ?? ''}</p>
         <button
           type='button'
           className='button button--secondary text-caption'
-          onClick={this.props.onToggleDrawer}
+          onClick={onToggleDrawer}
         >
           취소하기
         </button>
       </Drawer>,
-      this.drawerContainer
-    );
-  }
-}
+      document.getElementById('modal-hook') as HTMLDivElement
+    )
+  ) : null;
+};
 
-export default RestaurantDetailDrawer;
+export default React.memo(RestaurantDetailDrawer);
