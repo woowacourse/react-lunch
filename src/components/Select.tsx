e38667fc7c9a02c60b85e72cb49aa629ae8baa3b
cruelladevil <dev.timetravel@gmail.com>
2023-04-17 16:24:55 +0900

    refactor: Select 범용 컴포넌트로 수정

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,61 +1,24 @@
 import { ChangeEvent } from 'react';
-import { CATEGORIES, SORT_BY } from '../constants/constants';
-import type { Category, SortBy } from '../types/Restaurant';
 
 type SelectProps = {
-  setCategory: (newCategory: Category) => void;
-  setSort: (newSort: SortBy) => void;
+  name: string;
+  id: string;
+  className: string;
+  options: readonly string[];
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 };
 
-const isCategory = (category: string): category is Category => {
-  for (const c of CATEGORIES) {
-    if (c === category) return true;
-  }
-  return false;
-};
-
-const isSortBy = (sortBy: string): sortBy is SortBy => {
-  for (const s of SORT_BY) {
-    if (s === sortBy) return true;
-  }
-  return false;
-};
-
-function Select({ setCategory, setSort }: SelectProps) {
-  const onChangeCategory = (event: ChangeEvent<HTMLSelectElement>) => {
-    if (isCategory(event.target.value)) setCategory(event.target.value);
-  };
-  const onChangeSort = (event: ChangeEvent<HTMLSelectElement>) => {
-    if (isSortBy(event.target.value)) setSort(event.target.value);
-  };
-
+function Select({ name, id, className, options, onChange }: SelectProps) {
   return (
-    <section className="restaurant-filter-container">
-      <select
-        name="category"
-        id="category-filter"
-        className="restaurant-filter"
-        onChange={(event) => onChangeCategory(event)}
-      >
-        <option value="전체">전체</option>
-        <option value="한식">한식</option>
-        <option value="중식">중식</option>
-        <option value="일식">일식</option>
-        <option value="양식">양식</option>
-        <option value="아시안">아시안</option>
-        <option value="기타">기타</option>
-      </select>
-
-      <select
-        name="sorting"
-        id="sorting-filter"
-        className="restaurant-filter"
-        onChange={(event) => onChangeSort(event)}
-      >
-        <option value="이름순">이름순</option>
-        <option value="거리순">거리순</option>
-      </select>
-    </section>
+    <select name={name} id={id} className={className} onChange={onChange}>
+      {options.map((option) => {
+        return (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        );
+      })}
+    </select>
   );
 }
 
