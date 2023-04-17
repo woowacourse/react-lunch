9a7b906426d9f90692534b167a78cf0676881dc0
inyeong-kang <tomatorkd@naver.com>
2023-04-17 10:55:47 +0900

    refactor(data): components/App이 아닌 data/db 파일에서 localStorage에 데이터 있는지 확인

--- src/db/restaurants.ts
+++ src/db/restaurants.ts
@@ -1,18 +1,23 @@
 import { restaurantItemsKey } from '../constants';
 import { Restaurant } from './../types';
+import data from '../data/mockData.json';
 
 const db = {
   isRestaurantItemsExist() {
     return localStorage.getItem(restaurantItemsKey) !== null;
   },
 
-  setRestaurants(restaurants: Restaurant[]) {
+  setOriginRestaurantData(restaurants: Restaurant[]) {
     localStorage.setItem(restaurantItemsKey, JSON.stringify(restaurants));
   },
 
-  getRestaurants(): Restaurant[] {
-    if(this.isRestaurantItemsExist()) {}
-    return JSON.parse(localStorage.getItem(restaurantItemsKey) || '');
+  getOriginRestaurantData(): Restaurant[] {
+    if (this.isRestaurantItemsExist()) {
+      return JSON.parse(localStorage.getItem(restaurantItemsKey) || '');
+    }
+    const mockData = data.items as Restaurant[];
+    this.setOriginRestaurantData(mockData);
+    return mockData;
   },
 };
 
