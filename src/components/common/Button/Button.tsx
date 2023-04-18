3487ee772d015317e570699c4b44e6d4b1dd9238
sh981013s <sh981013s@gmail.com>
2023-04-18 20:17:14 +0900

    refactor: default parameter 부여

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,9 +4,9 @@ import * as styled from './Button.styles';
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   PropsWithChildren<{ variant?: 'primary' | 'outlined' }>;
 
-const Button = ({ children, variant, ...buttonProps }: ButtonProps) => {
+const Button = ({ children, variant = 'outlined', ...buttonProps }: ButtonProps) => {
   return (
-    <styled.Button {...buttonProps} $variant={variant ?? 'outlined'}>
+    <styled.Button {...buttonProps} $variant={variant}>
       {children}
     </styled.Button>
   );
