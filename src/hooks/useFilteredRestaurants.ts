ff35f5480263bd83ee4e76b9f26a2671cb036604
sh981013s <sh981013s@gmail.com>
2023-04-18 20:53:25 +0900

    refactor: non-null filters function custom hook 외부로 분리

--- src/hooks/useFilteredRestaurants.ts
+++ src/hooks/useFilteredRestaurants.ts
@@ -2,17 +2,17 @@ import { useMemo, useState } from 'react';
 import type Filter from '../types/Filter';
 import type Restaurant from '../types/Restaurant';
 
+const nonNullFilters = (
+  inputFilters: Array<Filter<Restaurant> | null>,
+): Array<Filter<Restaurant>> =>
+  inputFilters.filter((filter): filter is Filter<Restaurant> => filter !== null);
+
 const useFilteredRestaurants = (restaurants: Restaurant[]) => {
   const [sortFilter, setSortFilter] = useState<Filter<Restaurant> | null>(null);
   const [categoryFilter, setCategoryFilter] = useState<Filter<Restaurant> | null>(null);
 
   const filteredRestaurants = useMemo(() => {
     const filters = [sortFilter, categoryFilter];
-
-    const nonNullFilters = (
-      inputFilters: Array<Filter<Restaurant> | null>,
-    ): Array<Filter<Restaurant>> =>
-      inputFilters.filter((filter): filter is Filter<Restaurant> => filter !== null);
 
     return nonNullFilters(filters).reduce(
       (prevRestaurants, filter) => filter(prevRestaurants),
